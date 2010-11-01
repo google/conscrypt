d5d853ea85869524fbf35ca99b522d4c1a0bd6b0
Brian Carlstrom <bdc@google.com>
2010-11-01 14:19:59 -0700

    Avoid races between OpenSSLSocketImpl I/O and close()
    The previous change:
    
        commit 5f2e6872311240319509aed64d9f58cd5b64719b
        Author: Brian Carlstrom <bdc@google.com>
        Date:   Mon Aug 23 14:06:51 2010 -0700
    
        SSLSocket.read should throw SocketException not NullPointerException
    
    added checkOpen() to throw SocketException instead of
    NullPointerException, but there was still a race between read/write on
    one thread and close on another that could allow a
    NullPointerException to escape. This change moves checkOpen() calls to
    be protected by the existing writeLock/readLock/handshakeLock
    synchronzied blocks to avoid this case.
    
    byte buffer error checking for read/write is also moved into the to
    lock region to preserve compatability as measured by the test:
        libcore.javax.net.ssl.SSLSocketTest#test_SSLSocket_close
    
    Bug: 3153162

--- luni/src/main/java/org/apache/harmony/xnet/provider/jsse/OpenSSLSocketImpl.java
+++ luni/src/main/java/org/apache/harmony/xnet/provider/jsse/OpenSSLSocketImpl.java
@@ -345,8 +345,8 @@ public class OpenSSLSocketImpl
      * @param full If true, disable handshake cutthrough for a fully synchronous handshake
      */
     public synchronized void startHandshake(boolean full) throws IOException {
-        checkOpen();
         synchronized (handshakeLock) {
+            checkOpen();
             if (!handshakeStarted) {
                 handshakeStarted = true;
             } else {
@@ -757,9 +757,9 @@ public class OpenSSLSocketImpl
          */
         @Override
         public int read() throws IOException {
-            checkOpen();
             BlockGuard.getThreadPolicy().onNetwork();
             synchronized (readLock) {
+                checkOpen();
                 return NativeCrypto.SSL_read_byte(sslNativePointer, fd, OpenSSLSocketImpl.this,
                                                   getSoTimeout());
             }
@@ -771,18 +771,18 @@ public class OpenSSLSocketImpl
          */
         @Override
         public int read(byte[] b, int off, int len) throws IOException {
-            checkOpen();
             BlockGuard.getThreadPolicy().onNetwork();
-            if (b == null) {
-                throw new NullPointerException("b == null");
-            }
-            if ((len | off) < 0 || len > b.length - off) {
-                throw new IndexOutOfBoundsException();
-            }
-            if (0 == len) {
-                return 0;
-            }
             synchronized (readLock) {
+                checkOpen();
+                if (b == null) {
+                    throw new NullPointerException("b == null");
+                }
+                if ((len | off) < 0 || len > b.length - off) {
+                    throw new IndexOutOfBoundsException();
+                }
+                if (0 == len) {
+                    return 0;
+                }
                 return NativeCrypto.SSL_read(sslNativePointer, fd, OpenSSLSocketImpl.this,
                                              b, off, len, getSoTimeout());
             }
@@ -809,9 +809,9 @@ public class OpenSSLSocketImpl
          */
         @Override
         public void write(int b) throws IOException {
-            checkOpen();
             BlockGuard.getThreadPolicy().onNetwork();
             synchronized (writeLock) {
+                checkOpen();
                 NativeCrypto.SSL_write_byte(sslNativePointer, fd, OpenSSLSocketImpl.this, b);
             }
         }
@@ -822,18 +822,18 @@ public class OpenSSLSocketImpl
          */
         @Override
         public void write(byte[] b, int start, int len) throws IOException {
-            checkOpen();
             BlockGuard.getThreadPolicy().onNetwork();
-            if (b == null) {
-                throw new NullPointerException("b == null");
-            }
-            if ((len | start) < 0 || len > b.length - start) {
-                throw new IndexOutOfBoundsException();
-            }
-            if (len == 0) {
-                return;
-            }
             synchronized (writeLock) {
+                checkOpen();
+                if (b == null) {
+                    throw new NullPointerException("b == null");
+                }
+                if ((len | start) < 0 || len > b.length - start) {
+                    throw new IndexOutOfBoundsException();
+                }
+                if (len == 0) {
+                    return;
+                }
                 NativeCrypto.SSL_write(sslNativePointer, fd, OpenSSLSocketImpl.this, b, start, len);
             }
         }
