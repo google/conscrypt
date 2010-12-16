edc307bd5a7cf750852b9083ba203ba1c24fcdae
Brian Carlstrom <bdc@google.com>
2010-12-15 23:57:17 -0800

    HttpsURLConnection retry should not invoke X509TrustManager and HostnameVerifier more than once
    Summary:
    
    In 2.3, HttpsURLConnection was change to retry TLS connections as SSL
    connections w/o compression to deal with servers that are TLS
    intolerant. However, if the handshake proceeded to the point of
    invoking the X509TrustManager, we should not retry. Similarly, if we
    should not invoke the HostnameVerifier repeatedly, and need to wait
    until the SSL handshake has completed.
    
    Tested with (includes two new tests for this issue):
    	libcore/luni/src/test/java/libcore/javax/net/ssl/
    	libcore/luni/src/test/java/libcore/java/net/URLConnectionTest.java
    	libcore/luni/src/test/java/org/apache/harmony/luni/tests/internal/net/www/protocol/https/HttpsURLConnectionTest.java
    
    Details:
    
        HttpConnection.setupSecureSocket has been broken into two
        pieces. setupSecureSocket now just does the SSL
        handshaking. verifySecureSocketHostname now does the
        verification. The old HttpConnection code was careful never to
        assign its sslSocket field until verification was complete. A new
        unverifiedSocket field is added to store the sslSocket before
        verification is completed by verifySecureSocketHostname.
    
    	luni/src/main/java/org/apache/harmony/luni/internal/net/www/protocol/http/HttpConnection.java
    
        HttpsEngine.makeConnection now skips TLS intolerant retry if the
        reason for the makeSslConnection failure was a
        CertificateException, since that implies that we failed during
        certification validation after initial handshaking. We also
        prevent retrying hostname verification by moving it out of
        makeSslConnection and only doing it on new SSL connections,
        tracking the changes to HttpConnection.setupSecureSocket mentioned
        above. We also now skip the redundant call to setUpTransportIO in
        makeSslConnection on reused SSLSockets.
    
    	luni/src/main/java/org/apache/harmony/luni/internal/net/www/protocol/https/HttpsURLConnectionImpl.java
    
        Instead of throwing away the underlying CertificateExceptions, set
        them as the cause of the SSLExceptions. This is what the RI does
        in the case of X509TrustManager failures and is now used by
        HttpsEngine.makeConnection.
    
    	luni/src/main/java/org/apache/harmony/xnet/provider/jsse/OpenSSLSessionImpl.java
    	luni/src/main/java/org/apache/harmony/xnet/provider/jsse/OpenSSLSocketImpl.java
    
        Added new testConnectViaHttpsToUntrustedServer which makes sure
        that connections are not retried on certificate verification
        failure.
    
    	luni/src/test/java/libcore/java/net/URLConnectionTest.java
    
        Added new test_SSLSocket_untrustedServer that verifies that an
        SSLHandshakeException is thown containing a CertificateException
        is thrown on certificate verification problems.
    
    	luni/src/test/java/libcore/javax/net/ssl/SSLSocketTest.java
    
        Added second test CA and a new TestKeyStore.getClientCA2 test key
        store that does not trust the primary test key stores. This is
        useful for negative testing and is used in the above two new
        tests.
    
    	support/src/test/java/libcore/java/security/TestKeyStore.java
    
    Issue: http://code.google.com/p/android/issues/detail?id=13178
    Bug: 3292412
    
    Change-Id: I37136bb65f04d2bceaf2f32f542d6432c8b76ad4

--- luni/src/main/java/org/apache/harmony/xnet/provider/jsse/OpenSSLSocketImpl.java
+++ luni/src/main/java/org/apache/harmony/xnet/provider/jsse/OpenSSLSocketImpl.java
@@ -35,7 +35,7 @@ import java.util.logging.Logger;
 import javax.net.ssl.HandshakeCompletedEvent;
 import javax.net.ssl.HandshakeCompletedListener;
 import javax.net.ssl.SSLException;
-import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.SSLSession;
 import javax.security.auth.x500.X500Principal;
 import org.apache.harmony.security.provider.cert.X509CertImpl;
@@ -474,7 +474,9 @@ public class OpenSSLSocketImpl
             sslSessionNativePointer = NativeCrypto.SSL_do_handshake(sslNativePointer, fd, this,
                                                                     getSoTimeout(), client);
         } catch (CertificateException e) {
-            throw new SSLPeerUnverifiedException(e.getMessage());
+            SSLHandshakeException exception = new SSLHandshakeException(e.getMessage());
+            exception.initCause(e);
+            throw exception;
         }
         byte[] sessionId = NativeCrypto.SSL_SESSION_session_id(sslSessionNativePointer);
         sslSession = (OpenSSLSessionImpl) sessionContext.getSession(sessionId);
