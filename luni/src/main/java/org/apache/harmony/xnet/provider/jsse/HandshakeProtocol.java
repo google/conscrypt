a9de7ae71b2b17d29568f78105d58ee10cbb1ec9
Elliott Hughes <enh@google.com>
2011-01-11 18:15:55 -0800

    Change all "final static"s to "static final".
    Just so we sound like native speakers.
    
    Change-Id: I4d98ec7519af8c1578609945ca9d480484b82874

--- luni/src/main/java/org/apache/harmony/xnet/provider/jsse/HandshakeProtocol.java
+++ luni/src/main/java/org/apache/harmony/xnet/provider/jsse/HandshakeProtocol.java
@@ -43,22 +43,22 @@ public abstract class HandshakeProtocol {
     /**
      * Handshake status NEED_UNWRAP - HandshakeProtocol needs to receive data
      */
-    public final static int NEED_UNWRAP = 1;
+    public static final int NEED_UNWRAP = 1;
 
     /**
      * Handshake status NOT_HANDSHAKING - is not currently handshaking
      */
-    public final static int NOT_HANDSHAKING = 2;
+    public static final int NOT_HANDSHAKING = 2;
 
     /**
      * Handshake status FINISHED - HandshakeProtocol has just finished
      */
-    public final static int FINISHED = 3;
+    public static final int FINISHED = 3;
 
     /**
      * Handshake status NEED_TASK - HandshakeProtocol needs the results of delegated task
      */
-    public final static int NEED_TASK = 4;
+    public static final int NEED_TASK = 4;
 
     /**
      * Current handshake status
