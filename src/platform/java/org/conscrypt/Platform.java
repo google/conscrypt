35b1f354ec2b647966a198ffed932d82eb8eeb5b
rich cannings <richc@google.com>
2014-06-27 10:48:16 -0700

    Log CCS exceptions
    Unlike the previous CL, this uses reflection for android.os.Process and
    android.util.EventLog throughout.
    
    Bug: 15452942
    Change-Id: I34b9eaedf1f1e450b1f8004887bb0482601d789e

--- src/platform/java/org/conscrypt/Platform.java
+++ src/platform/java/org/conscrypt/Platform.java
@@ -26,6 +26,7 @@ import android.system.Os;
 import android.system.StructTimeval;
 import java.io.FileDescriptor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketImpl;
@@ -121,5 +122,26 @@ class Platform {
      */
     public static OpenSSLKey wrapRsaKey(PrivateKey key) {
         return null;
+    }
+
+    /**
+     * Logs to the system EventLog system.
+     */
+    public static void logEvent(String message) {
+        try {
+            Class processClass = Class.forName("android.os.Process");
+            Object processInstance = processClass.newInstance();
+            Method myUidMethod = processClass.getMethod("myUid", (Class[]) null);
+            int uid = (Integer) myUidMethod.invoke(processInstance);
+
+            Class eventLogClass = Class.forName("android.util.EventLog");
+            Object eventLogInstance = eventLogClass.newInstance();
+            Method writeEventMethod = eventLogClass.getMethod("writeEvent",
+                    new Class[] { Integer.TYPE, Object[].class });
+            writeEventMethod.invoke(eventLogInstance, 0x534e4554 /* SNET */,
+                    new Object[] { "conscrypt", uid, message });
+        } catch (Exception e) {
+            // Do not log and fail silently
+        }
     }
 }
