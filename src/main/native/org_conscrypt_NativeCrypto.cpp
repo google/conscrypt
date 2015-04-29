cf9111e5e6a7bb2e777c1b4873732c4196889194
Kenny Root <kroot@google.com>
2015-04-29 10:08:33 -0700

    NativeCrypto: compatibility with OpenSSL
    OpenSSL has different definitions for the ECDSA functions which causes
    compilation errors when we're being strict. Sprinkle in some more #ifdef
    to allow compilation for both.
    
    Change-Id: Iafe3799289980fdf7862a861b2aee4517dbd869a

--- src/main/native/org_conscrypt_NativeCrypto.cpp
+++ src/main/native/org_conscrypt_NativeCrypto.cpp
@@ -1636,7 +1636,11 @@ void ExDataFree(void* /* parent */,
                 CRYPTO_EX_DATA* ad,
                 int idx,
                 long /* argl */,
+#if defined(OPENSSL_IS_BORINGSSL)
                 const void* /* argp */) {
+#else /* defined(OPENSSL_IS_BORINGSSL) */
+                void* /* argp */) {
+#endif /* defined(OPENSSL_IS_BORINGSSL) */
     jobject private_key = reinterpret_cast<jobject>(ptr);
     if (private_key == NULL) return;
 
@@ -1650,7 +1654,11 @@ int ExDataDup(CRYPTO_EX_DATA* /* to */,
               void* /* from_d */,
               int /* idx */,
               long /* argl */,
+#if defined(OPENSSL_IS_BORINGSSL)
               const void* /* argp */) {
+#else /* defined(OPENSSL_IS_BORINGSSL) */
+              void* /* argp */) {
+#endif /* defined(OPENSSL_IS_BORINGSSL) */
     // This callback shall never be called with the current OpenSSL
     // implementation (the library only ever duplicates EX_DATA items
     // for SSL and BIO objects). But provide this to catch regressions
