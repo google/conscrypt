203afd8175c40c8a5a74bb7fd070eda04063910b
Kenny Root <kroot@google.com>
2015-05-18 16:36:23 -0700

    Fix up JNI_TRACE for AEAD
    (cherry picked from commit edc4f273c9c1c862119cdce3e4c9ca15110b2e1f)
    
    Bug: 21762837
    Change-Id: I11042be8fe1e046ac96759b4554ce9229e1cf6f3

--- src/main/native/org_conscrypt_NativeCrypto.cpp
+++ src/main/native/org_conscrypt_NativeCrypto.cpp
@@ -5168,8 +5168,8 @@ static jint evp_aead_ctx_op(JNIEnv* env, jobject ctxRef, jbyteArray outArray, ji
     }
 
     if (ARRAY_OFFSET_INVALID(outBytes, outOffset)) {
-        JNI_TRACE("evp_aead_ctx_op(%p, %p, %d, %p, %d, %d, %p)", ctx, outArray, outOffset,
-                nonceArray, inArray, inOffset, inLength, aadArray);
+        JNI_TRACE("evp_aead_ctx_op(%p, %p, %d, %p, %p, %d, %d, %p)", ctx, outArray, outOffset,
+                  nonceArray, inArray, inOffset, inLength, aadArray);
         jniThrowException(env, "java/lang/ArrayIndexOutOfBoundsException", "out");
         return 0;
     }
@@ -5180,8 +5180,8 @@ static jint evp_aead_ctx_op(JNIEnv* env, jobject ctxRef, jbyteArray outArray, ji
     }
 
     if (ARRAY_OFFSET_LENGTH_INVALID(inBytes, inOffset, inLength)) {
-        JNI_TRACE("evp_aead_ctx_op(%p, %p, %d, %p, %d, %d, %p)", ctx, outArray, outOffset,
-                nonceArray, inArray, inOffset, inLength, aadArray);
+        JNI_TRACE("evp_aead_ctx_op(%p, %p, %d, %p, %p, %d, %d, %p)", ctx, outArray, outOffset,
+                  nonceArray, inArray, inOffset, inLength, aadArray);
         jniThrowException(env, "java/lang/ArrayIndexOutOfBoundsException", "in");
         return 0;
     }
