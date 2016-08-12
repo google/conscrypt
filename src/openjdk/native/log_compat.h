e939897c4bb292d158ed4491fad9d0f256955c2d
Kenny Root <kroot@google.com>
2016-08-12 01:19:55 -0700

    Add unused var args macro so OpenJDK compiles with debug
    Currently the OpenJDK macros are not hooked up to any logging function,
    so they fail to compile with all the strict checking enabled.
    
    Test: enable all WITH_JNI_TRACE; mmma -j32 external/conscrypt
    Change-Id: Ia71296962940eca6afe934a20ffda054760b0907

--- src/openjdk/native/log_compat.h
+++ src/openjdk/native/log_compat.h
@@ -1,6 +1,33 @@
+/*
+ * Copyright 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _CONSCRYPT_LOG_COMPAT_H
+#define _CONSCRYPT_LOG_COMPAT_H
+
+#include "unused.h"
+
+#define LOG_INFO ((void)0)
+
+#define ALOG(...) \
+            VA_ARGS_UNUSED(__VA_ARGS__)
 #define ALOGD(...) \
-            /* __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__) */
+            VA_ARGS_UNUSED(__VA_ARGS__)
 #define ALOGE(...) \
-            /* __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__) */
+            VA_ARGS_UNUSED(__VA_ARGS__)
 #define ALOGV(...) \
-            /* __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__) */
+            VA_ARGS_UNUSED(__VA_ARGS__)
+
+#endif /* _CONSCRYPT_LOG_COMPAT_H */
