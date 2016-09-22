adb03d62f27fb627651de1db1de5f8ce54771b0c
Kenny Root <kroot@google.com>
2016-09-22 16:46:32 -0700

    Remove some old Makefile cruft
    Test: mmma -j32 external/conscrypt
    Change-Id: Ic0095be8c15839746925d16255787851f9388e4b

--- Android.mk
+++ Android.mk
@@ -46,11 +46,6 @@ local_javac_flags:=-Xmaxwarns 9999999
 core_cflags := -Wall -Wextra -Werror -Wunused
 core_cppflags := -Wall -Wextra -Werror -Wunused
 
-# libnativehelper's UniquePtr is deprecated but changing to std::unique_ptr has
-# been rejected here in the past:
-# https://android-review.googlesource.com/#/c/113096/
-core_cppflags += -Wno-error=deprecated-declarations
-
 #
 # Build for the target (device).
 #
@@ -81,7 +76,6 @@ LOCAL_JARJAR_RULES := $(LOCAL_PATH)/jarjar-rules.txt
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := conscrypt
 LOCAL_REQUIRED_MODULES := libjavacrypto
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 include $(BUILD_JAVA_LIBRARY)
 
@@ -95,7 +89,6 @@ LOCAL_NO_STANDARD_LIBRARIES := true
 LOCAL_JAVACFLAGS := $(local_javac_flags)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := conscrypt-nojarjar
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
@@ -112,7 +105,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := conscrypt-tests
 LOCAL_REQUIRED_MODULES := libjavacrypto
 LOCAL_JARJAR_RULES := $(LOCAL_PATH)/jarjar-rules.txt
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 include $(BUILD_STATIC_JAVA_LIBRARY)
 endif
@@ -130,10 +122,9 @@ LOCAL_C_INCLUDES += \
         libcore/include \
         libcore/luni/src/main/native \
         $(LOCAL_PATH)/src/platform/native
-LOCAL_SHARED_LIBRARIES := libcrypto libjavacore liblog libnativehelper libssl libz
+LOCAL_SHARED_LIBRARIES := libcrypto libjavacore liblog libnativehelper libssl
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libjavacrypto
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 include $(BUILD_SHARED_LIBRARY)
 
 # Unbundled Conscrypt jar
@@ -146,7 +137,6 @@ LOCAL_JAVACFLAGS := $(local_javac_flags)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := conscrypt_unbundled
 LOCAL_JAVA_LIBRARIES := conscrypt-stubs
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_JACK_FLAGS := -D jack.classpath.default-libraries=false
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 include $(BUILD_STATIC_JAVA_LIBRARY)
@@ -172,11 +162,10 @@ LOCAL_C_INCLUDES += \
         external/openssl/include \
         external/openssl \
         $(LOCAL_PATH)/src/compat/native
-LOCAL_LDFLAGS := -llog -lz -ldl
+LOCAL_LDFLAGS := -llog -ldl
 LOCAL_STATIC_LIBRARIES := libssl libcrypto
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libconscrypt_jni
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_SDK_VERSION := 9
 LOCAL_NDK_STL_VARIANT := c++_shared
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
@@ -198,7 +187,6 @@ LOCAL_C_INCLUDES += \
         $(LOCAL_PATH)/src/compat/native
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libconscrypt_static
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_STATIC_LIBRARIES := libssl libcrypto
 LOCAL_SDK_VERSION := 9
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
@@ -221,7 +209,6 @@ LOCAL_JARJAR_RULES := $(LOCAL_PATH)/jarjar-rules.txt
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := conscrypt-hostdex
 LOCAL_REQUIRED_MODULES := libjavacrypto
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 include $(BUILD_HOST_DALVIK_JAVA_LIBRARY)
 
@@ -234,7 +221,6 @@ LOCAL_JAVACFLAGS := $(local_javac_flags)
 LOCAL_BUILD_HOST_DEX := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := conscrypt-hostdex-nojarjar
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 include $(BUILD_HOST_DALVIK_JAVA_LIBRARY)
 
@@ -248,7 +234,6 @@ ifeq ($(LIBCORE_SKIP_TESTS),)
     LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE := conscrypt-tests-hostdex
     LOCAL_REQUIRED_MODULES := libjavacrypto
-    LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
     LOCAL_JAVA_LANGUAGE_VERSION := 1.7
     include $(BUILD_HOST_DALVIK_JAVA_LIBRARY)
 endif
@@ -269,7 +254,6 @@ LOCAL_LDLIBS += -lpthread
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libjavacrypto
 LOCAL_CFLAGS += -DJNI_JARJAR_PREFIX="com/android/"
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_SHARED_LIBRARIES := libcrypto libjavacore liblog libnativehelper libssl
 LOCAL_MULTILIB := both
 include $(BUILD_HOST_SHARED_LIBRARY)
@@ -291,7 +275,6 @@ ifeq (,$(TARGET_BUILD_APPS))
     LOCAL_LDLIBS += -lpthread
     LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE := libconscrypt_jni
-    LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
     LOCAL_SHARED_LIBRARIES := libcrypto libjavacore liblog libnativehelper libssl
     LOCAL_MULTILIB := both
     include $(BUILD_HOST_SHARED_LIBRARY)
@@ -315,7 +298,6 @@ LOCAL_CPPFLAGS += $(core_cppflags) -DCONSCRYPT_OPENJDK
 LOCAL_LDLIBS += -lpthread
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libconscrypt_openjdk_jni
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_CXX_STL := libc++_static
 LOCAL_STATIC_LIBRARIES := libssl_static-host libcrypto_static
 LOCAL_MULTILIB := both
@@ -336,7 +318,6 @@ LOCAL_GENERATED_SOURCES := $(conscrypt_gen_java_files)
 LOCAL_JAVACFLAGS := $(local_javac_flags) -XDignore.symbol.file
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := conscrypt-host
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 include $(BUILD_HOST_JAVA_LIBRARY)
 
