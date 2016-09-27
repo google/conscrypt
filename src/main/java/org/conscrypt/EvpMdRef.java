f58eccb78d99dd1fe2f58c11b5fa805edf0d700d
Kenny Root <kroot@google.com>
2016-09-27 13:28:35 -0700

    Move JCA names and OIDs to constants
    Test: cts-tradefed run cts -m CtsLibcoreTestCases -a arm64-v8a
    Change-Id: I2fb67d6e9aa812b3b2ea26e14d18fbe752c70fc3

--- src/main/java/org/conscrypt/EvpMdRef.java
+++ src/main/java/org/conscrypt/EvpMdRef.java
@@ -17,6 +17,7 @@
 package org.conscrypt;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
 
 public final class EvpMdRef {
     /**
@@ -24,20 +25,20 @@ public final class EvpMdRef {
      * algorithm name or {@code null} if the digest algorithm is not known.
      */
     public static String getJcaDigestAlgorithmStandardName(String algorithm) {
-        if (("SHA-256".equalsIgnoreCase(algorithm))
-                || ("2.16.840.1.101.3.4.2.1".equals(algorithm))) {
-            return "SHA-256";
-        } else if (("SHA-512".equalsIgnoreCase(algorithm))
-                || ("2.16.840.1.101.3.4.2.3".equals(algorithm))) {
-            return "SHA-512";
-        } else if (("SHA-1".equalsIgnoreCase(algorithm)) || ("1.3.14.3.2.26".equals(algorithm))) {
-            return "SHA-1";
-        } else if (("SHA-384".equalsIgnoreCase(algorithm))
-                || ("2.16.840.1.101.3.4.2.2".equals(algorithm))) {
-            return "SHA-384";
-        } else if (("SHA-224".equalsIgnoreCase(algorithm))
-                || ("2.16.840.1.101.3.4.2.4".equals(algorithm))) {
-            return "SHA-224";
+        String algorithmUpper = algorithm.toUpperCase(Locale.US);
+        if ((SHA256.JCA_NAME.equals(algorithmUpper)) || (SHA256.OID.equals(algorithmUpper))) {
+            return SHA256.JCA_NAME;
+        } else if ((SHA512.JCA_NAME.equals(algorithmUpper))
+                || (SHA512.OID.equals(algorithmUpper))) {
+            return SHA512.JCA_NAME;
+        } else if ((SHA1.JCA_NAME.equals(algorithmUpper)) || (SHA1.OID.equals(algorithmUpper))) {
+            return SHA1.JCA_NAME;
+        } else if ((SHA384.JCA_NAME.equals(algorithmUpper))
+                || (SHA384.OID.equals(algorithmUpper))) {
+            return SHA384.JCA_NAME;
+        } else if ((SHA224.JCA_NAME.equals(algorithmUpper))
+                || (SHA224.OID.equals(algorithmUpper))) {
+            return SHA224.JCA_NAME;
         } else {
             return null;
         }
@@ -45,15 +46,16 @@ public final class EvpMdRef {
 
     public static long getEVP_MDByJcaDigestAlgorithmStandardName(String algorithm)
             throws NoSuchAlgorithmException {
-        if ("SHA-256".equalsIgnoreCase(algorithm)) {
+        String algorithmUpper = algorithm.toUpperCase(Locale.US);
+        if (SHA256.JCA_NAME.equals(algorithmUpper)) {
             return EvpMdRef.SHA256.EVP_MD;
-        } else if ("SHA-512".equalsIgnoreCase(algorithm)) {
+        } else if (SHA512.JCA_NAME.equals(algorithmUpper)) {
             return EvpMdRef.SHA512.EVP_MD;
-        } else if ("SHA-1".equalsIgnoreCase(algorithm)) {
+        } else if (SHA1.JCA_NAME.equals(algorithmUpper)) {
             return EvpMdRef.SHA1.EVP_MD;
-        } else if ("SHA-384".equalsIgnoreCase(algorithm)) {
+        } else if (SHA384.JCA_NAME.equals(algorithmUpper)) {
             return EvpMdRef.SHA384.EVP_MD;
-        } else if ("SHA-224".equalsIgnoreCase(algorithm)) {
+        } else if (SHA224.JCA_NAME.equals(algorithmUpper)) {
             return EvpMdRef.SHA224.EVP_MD;
         } else {
             throw new NoSuchAlgorithmException("Unsupported algorithm: " + algorithm);
@@ -61,31 +63,43 @@ public final class EvpMdRef {
     }
 
     public static final class MD5 {
+        public static final String JCA_NAME = "MD5";
+        public static final String OID = "1.2.840.113549.2.5";
         public static final long EVP_MD = NativeCrypto.EVP_get_digestbyname("md5");
         public static final int SIZE = NativeCrypto.EVP_MD_size(EVP_MD);
     }
 
     public static final class SHA1 {
+        public static final String JCA_NAME = "SHA-1";
+        public static final String OID = "1.3.14.3.2.26";
         public static final long EVP_MD = NativeCrypto.EVP_get_digestbyname("sha1");
         public static final int SIZE = NativeCrypto.EVP_MD_size(EVP_MD);
     }
 
     public static final class SHA224 {
+        public static final String JCA_NAME = "SHA-224";
+        public static final String OID = "2.16.840.1.101.3.4.2.4";
         public static final long EVP_MD = NativeCrypto.EVP_get_digestbyname("sha224");
         public static final int SIZE = NativeCrypto.EVP_MD_size(EVP_MD);
     }
 
     public static final class SHA256 {
+        public static final String JCA_NAME = "SHA-256";
+        public static final String OID = "2.16.840.1.101.3.4.2.1";
         public static final long EVP_MD = NativeCrypto.EVP_get_digestbyname("sha256");
         public static final int SIZE = NativeCrypto.EVP_MD_size(EVP_MD);
     }
 
     public static final class SHA384 {
+        public static final String JCA_NAME = "SHA-384";
+        public static final String OID = "2.16.840.1.101.3.4.2.2";
         public static final long EVP_MD = NativeCrypto.EVP_get_digestbyname("sha384");
         public static final int SIZE = NativeCrypto.EVP_MD_size(EVP_MD);
     }
 
     public static final class SHA512 {
+        public static final String JCA_NAME = "SHA-512";
+        public static final String OID = "2.16.840.1.101.3.4.2.3";
         public static final long EVP_MD = NativeCrypto.EVP_get_digestbyname("sha512");
         public static final int SIZE = NativeCrypto.EVP_MD_size(EVP_MD);
     }
