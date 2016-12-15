google/conscrypt

commit 7782d132fab6ac9f30b97d76ed3a51126ed3e6d1

Restructuring conscrypt into submodules
Change-Id: I2a4dea5d2a5f35b29fdbb96bcdae1888a4fe2b7e

common/src/jni/main/include/ByteArray.h
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef BYTE_ARRAY_H_included
+#define BYTE_ARRAY_H_included
+
+// Returns true if the VM's JNI GetByteArrayElements method is likely to create a copy when invoked
+// on an array of the provided size.
+bool isGetByteArrayElementsLikelyToReturnACopy(size_t size) {
+#ifdef ANDROID
+    // ART's GetByteArrayElements creates copies only for arrays smaller than 12 kB.
+    return size <= 12 * 1024;
+#else
+    (void) size;
+    // On OpenJDK based VMs GetByteArrayElements appears to always create a copy.
+    return true;
+#endif
+}
+
+#endif  // BYTE_ARRAY_H_included

src/compat/native/ByteArray.h
@@ -1,27 +1,0 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#ifndef BYTE_ARRAY_H_included
-#define BYTE_ARRAY_H_included
-
-// Returns true if the VM's JNI GetByteArrayElements method is likely to create a copy when invoked
-// on an array of the provided size.
-bool isGetByteArrayElementsLikelyToReturnACopy(size_t size) {
-    // ART's GetByteArrayElements creates copies only for arrays smaller than 12 kB.
-    return size <= 12 * 1024;
-}
-
-#endif  // BYTE_ARRAY_H_included

src/openjdk/native/ByteArray.h
@@ -1,27 +1,0 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#ifndef BYTE_ARRAY_H_included
-#define BYTE_ARRAY_H_included
-
-// Returns true if the VM's JNI GetByteArrayElements method is likely to create a copy when invoked
-// on an array of the provided size.
-bool isGetByteArrayElementsLikelyToReturnACopy(size_t /* size */) {
-    // On OpenJDK based VMs GetByteArrayElements appears to always create a copy.
-    return true;
-}
-
-#endif  // BYTE_ARRAY_H_included

src/platform/native/ByteArray.h
@@ -1,27 +1,0 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#ifndef BYTE_ARRAY_H_included
-#define BYTE_ARRAY_H_included
-
-// Returns true if the VM's JNI GetByteArrayElements method is likely to create a copy when invoked
-// on an array of the provided size.
-bool isGetByteArrayElementsLikelyToReturnACopy(size_t size) {
-    // ART's GetByteArrayElements creates copies only for arrays smaller than 12 kB.
-    return size <= 12 * 1024;
-}
-
-#endif  // BYTE_ARRAY_H_included

commit 07035eae39c353c079c6a18b35d30928d2a492c7

Fix OpenJDK check after refactor
Restructuring of the repository in commit
7782d132fab6ac9f30b97d76ed3a51126ed3e6d1 accidentally removed an
optimization added in commit e9b2cd57f0e40cc3021f0f6371cc103cab88f0cf.

This caused a performance regression in signapk when compiled in the
Android tree.

Test: make -j31
Bug: 33652343
Change-Id: Ib2d1bfee17ce7653a16d688084fdd981d8bc6141

common/src/jni/main/include/ByteArray.h
@@ -20,7 +20,7 @@
 // Returns true if the VM's JNI GetByteArrayElements method is likely to create a copy when invoked
 // on an array of the provided size.
 bool isGetByteArrayElementsLikelyToReturnACopy(size_t size) {
-#ifdef ANDROID
+#if defined(ANDROID) && !defined(CONSCRYPT_OPENJDK)
     // ART's GetByteArrayElements creates copies only for arrays smaller than 12 kB.
     return size <= 12 * 1024;
 #else