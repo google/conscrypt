763168f9cbc154c04ebab5ceb8e9139adaa5bdb5
Kenny Root <kroot@google.com>
2017-01-17 11:24:25 -0800

    Do not try to loadLibrary for GmsCore
    GmsCore loads the JNI libraries before calling into the Java code, so
    do not bother trying to call System.loadLibrary in Conscrypt.

--- android/src/main/java/org/conscrypt/NativeCryptoJni.java
+++ android/src/main/java/org/conscrypt/NativeCryptoJni.java
@@ -22,10 +22,7 @@ package org.conscrypt;
  */
 class NativeCryptoJni {
     public static void init() {
-        if ("com.google.android.gms.org.conscrypt".equals(NativeCrypto.class.getPackage().getName())) {
-            System.loadLibrary("gmscore");
-            System.loadLibrary("conscrypt_gmscore_jni");
-        } else {
+        if (!"com.google.android.gms.org.conscrypt".equals(NativeCrypto.class.getPackage().getName())) {
             System.loadLibrary("conscrypt_jni");
         }
     }
