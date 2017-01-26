f126cdaab00fdb37d148d7c1e4229e84e25a59fe
Kenny Root <kroot@google.com>
2017-01-26 15:12:07 -0800

    Eliminate some unneeded complexity in platform build
    All the files compile whether unbundled or not, so we can eliminate the
    switch statements in the build logic and just have it included
    everywhere.
    
    Change-Id: I7b6df16ad90a355f8e04bef27274a02806230ca9

--- Android.mk
+++ Android.mk
@@ -77,11 +77,6 @@ common_java_files := $(filter-out \
 	%/org/conscrypt/NativeCryptoJni.java \
 	, $(call all-java-files-under,common/src/main/java))
 
-bundled_main_cpp_files := \
-	common/src/jni/main/cpp/CompatibilityCloseMonitor.cpp \
-	common/src/jni/main/cpp/JniConstants.cpp \
-	common/src/jni/main/cpp/NativeCrypto.cpp
-
 # Create the conscrypt library
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(common_java_files)
@@ -140,7 +135,7 @@ include $(CLEAR_VARS)
 LOCAL_CFLAGS += $(core_cflags)
 LOCAL_CFLAGS += -DJNI_JARJAR_PREFIX="com/android/"
 LOCAL_CPPFLAGS += $(core_cppflags)
-LOCAL_SRC_FILES := $(bundled_main_cpp_files)
+LOCAL_SRC_FILES := $(call all-cpp-files-under,common/src/jni/main/cpp)
 LOCAL_C_INCLUDES += \
         external/openssl/include \
         external/openssl \
@@ -183,7 +178,7 @@ LOCAL_CPPFLAGS += $(core_cppflags) \
         -DJNI_JARJAR_PREFIX="com/google/android/gms/" \
         -DCONSCRYPT_UNBUNDLED \
         -DSTATIC_LIB
-LOCAL_SRC_FILES := $(bundled_main_cpp_files)
+LOCAL_SRC_FILES := $(call all-cpp-files-under,common/src/jni/main/cpp)
 LOCAL_C_INCLUDES += \
         external/openssl/include \
         external/openssl \
@@ -247,7 +242,7 @@ endif
 # Conscrypt native library for host
 include $(CLEAR_VARS)
 LOCAL_CLANG := true
-LOCAL_SRC_FILES += $(bundled_main_cpp_files)
+LOCAL_SRC_FILES := $(call all-cpp-files-under,common/src/jni/main/cpp)
 LOCAL_C_INCLUDES += \
         external/openssl/include \
         external/openssl \
@@ -283,5 +278,4 @@ conscrypt_generate_constants_exe :=
 core_cflags :=
 core_cppflags :=
 local_javac_flags :=
-bundled_main_cpp_files :=
 bundled_test_java_files :=
