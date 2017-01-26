google/conscrypt

commit 1fa6994406158e6391b5d8b576a6a8aa5a52d13d

Sanity checking during restore of SSL sessions
It's possible that corrupt data could cause an OOM here if the prefix
length read in is some large number. Do some sanity checking of incoming
data to avoid large allocations.

Test: cts-tradefed run cts -m CtsLibcoreOkHttpTestCases -a arm64-v8a
Test: cts-tradefed run cts -m CtsLibcoreTestCases -a arm64-v8a
Bug: 33162507
Change-Id: Iea7af3c97d9888d5ee63a3c87de2a8d5798fa20b

common/src/main/java/org/conscrypt/AbstractSessionContext.java
@@ -16,11 +16,11 @@
 
 package org.conscrypt;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
@@ -259,31 +259,45 @@ abstract class AbstractSessionContext implements SSLSessionContext {
         }
     }
 
+    private static void checkRemaining(ByteBuffer buf, int length) throws IOException {
+        if (length < 0) {
+            throw new IOException("Length is negative: " + length);
+        }
+        if (length > buf.remaining()) {
+            throw new IOException(
+                    "Length of blob is longer than available: " + length + " > " + buf.remaining());
+        }
+    }
+
     /**
      * Creates a session from the given bytes.
      *
      * @return a session or null if the session can't be converted
      */
-    OpenSSLSessionImpl toSession(byte[] data, String host, int port) {
-        ByteArrayInputStream bais = new ByteArrayInputStream(data);
-        DataInputStream dais = new DataInputStream(bais);
+    public OpenSSLSessionImpl toSession(byte[] data, String host, int port) {
+        ByteBuffer buf = ByteBuffer.wrap(data);
         try {
-            int type = dais.readInt();
+            int type = buf.get();
             if (type != OPEN_SSL && type != OPEN_SSL_WITH_OCSP && type != OPEN_SSL_WITH_TLS_SCT) {
-                log(new AssertionError("Unexpected type ID: " + type));
-                return null;
+                throw new IOException("Unexpected type ID: " + type);
             }
 
-            int length = dais.readInt();
-            byte[] sessionData = new byte[length];
-            dais.readFully(sessionData);
+            int length = buf.getInt();
+            checkRemaining(buf, length);
 
-            int count = dais.readInt();
+            byte[] sessionData = new byte[length];
+            buf.get(sessionData);
+
+            int count = buf.getInt();
+            checkRemaining(buf, count);
+
             X509Certificate[] certs = new X509Certificate[count];
             for (int i = 0; i < count; i++) {
-                length = dais.readInt();
+                length = buf.getInt();
+                checkRemaining(buf, length);
+
                 byte[] certData = new byte[length];
-                dais.readFully(certData);
+                buf.get(certData);
                 certs[i] = OpenSSLX509Certificate.fromX509Der(certData);
             }
 
@@ -291,26 +305,47 @@ abstract class AbstractSessionContext implements SSLSessionContext {
             if (type >= OPEN_SSL_WITH_OCSP) {
                 // We only support one OCSP response now, but in the future
                 // we may support RFC 6961 which has multiple.
-                int countOcspResponses = dais.readInt();
-                if (countOcspResponses > 1) {
-                    int ocspLength = dais.readInt();
+                int countOcspResponses = buf.getInt();
+                checkRemaining(buf, countOcspResponses);
+
+                if (countOcspResponses >= 1) {
+                    int ocspLength = buf.getInt();
+                    checkRemaining(buf, ocspLength);
+
                     ocspData = new byte[ocspLength];
-                    dais.readFully(ocspData);
+                    buf.get(ocspData);
+
+                    // Skip the rest of the responses.
+                    for (int i = 1; i < countOcspResponses; i++) {
+                        ocspLength = buf.getInt();
+                        checkRemaining(buf, ocspLength);
+                        buf.position(buf.position() + ocspLength);
+                    }
                 }
             }
 
             byte[] tlsSctData = null;
             if (type == OPEN_SSL_WITH_TLS_SCT) {
-                int tlsSctDataLength = dais.readInt();
+                int tlsSctDataLength = buf.getInt();
+                checkRemaining(buf, tlsSctDataLength);
+
                 if (tlsSctDataLength > 0) {
                     tlsSctData = new byte[tlsSctDataLength];
-                    dais.readFully(tlsSctData);
+                    buf.get(tlsSctData);
                 }
+            }
+
+            if (buf.remaining() != 0) {
+                log(new AssertionError("Read entire session, but data still remains; rejecting"));
+                return null;
             }
 
             return new OpenSSLSessionImpl(sessionData, host, port, certs, ocspData, tlsSctData,
                     this);
         } catch (IOException e) {
+            log(e);
+            return null;
+        } catch (BufferUnderflowException e) {
             log(e);
             return null;
         }
@@ -352,7 +387,8 @@ abstract class AbstractSessionContext implements SSLSessionContext {
     }
 
     static void log(Throwable t) {
-        new Exception("Error converting session", t).printStackTrace();
+        System.out.println("Error inflating SSL session: "
+                + (t.getMessage() != null ? t.getMessage() : t.getClass().getName()));
     }
 
     @Override

platform/src/test/java/org/conscrypt/TestSessionBuilder.java
@@ -1,0 +1,163 @@
+/*
+ * Copyright 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.conscrypt;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+
+public class TestSessionBuilder {
+    private int type;
+
+    private boolean sessionDataSet;
+    private byte[] sessionData;
+    private int sessionDataLength;
+
+    private boolean certificatesSet;
+    private ArrayList<byte[]> certificates = new ArrayList<>();
+    private int certificatesLength;
+    private ArrayList<Integer> certificateLengths = new ArrayList<>();
+
+    private boolean ocspDataSet;
+    private ArrayList<byte[]> ocspDatas = new ArrayList<>();
+    private int ocspDatasLength;
+    private ArrayList<Integer> ocspDataLengths = new ArrayList<>();
+
+    private boolean tlsSctDataSet;
+    private byte[] tlsSctData;
+    private int tlsSctDataLength;
+
+    public TestSessionBuilder setType(int type) {
+        this.type = type;
+        return this;
+    }
+
+    public TestSessionBuilder setSessionData(byte[] sessionData) {
+        sessionDataSet = true;
+        this.sessionData = sessionData;
+        sessionDataLength = sessionData.length;
+        return this;
+    }
+
+    public TestSessionBuilder setSessionDataLength(int sessionDataLength) {
+        assertTrue("call setSessionData first", sessionDataSet);
+        this.sessionDataLength = sessionDataLength;
+        return this;
+    }
+
+    public TestSessionBuilder addCertificate(byte[] certificate) {
+        certificatesSet = true;
+        certificates.add(certificate);
+        certificateLengths.add(certificate.length);
+        certificatesLength = certificates.size();
+        return this;
+    }
+
+    public TestSessionBuilder setCertificatesLength(int certificatesLength) {
+        assertTrue("call addCertificate first", certificatesSet);
+        this.certificatesLength = certificatesLength;
+        return this;
+    }
+
+    public TestSessionBuilder setCertificateLength(int certIndex, int certLength) {
+        assertTrue("call addCertificate first", certificatesSet);
+        certificateLengths.set(certIndex, certLength);
+        return this;
+    }
+
+    public TestSessionBuilder setOcspDataEmpty() {
+        ocspDataSet = true;
+        return this;
+    }
+
+    public TestSessionBuilder addOcspData(byte[] ocspData) {
+        ocspDataSet = true;
+        ocspDatas.add(ocspData);
+        ocspDataLengths.add(ocspData.length);
+        ocspDatasLength = ocspDatas.size();
+        return this;
+    }
+
+    public TestSessionBuilder setOcspDatasLength(int ocspDatasLength) {
+        assertTrue("Call addOcspData before setting length", ocspDataSet);
+        this.ocspDatasLength = ocspDatasLength;
+        return this;
+    }
+
+    public TestSessionBuilder setOcspDataLength(int ocspDataIndex, int ocspDataLength) {
+        assertTrue("Call addOcspData before setting length", ocspDataSet);
+        this.ocspDataLengths.set(ocspDataIndex, ocspDataLength);
+        return this;
+    }
+
+    public TestSessionBuilder setTlsSctData(byte[] tlsSctData) {
+        tlsSctDataSet = true;
+        this.tlsSctData = tlsSctData.clone();
+        tlsSctDataLength = tlsSctData.length;
+        return this;
+    }
+
+    public TestSessionBuilder setTlsSctDataLength(int tlsSctDataLength) {
+        assertTrue("Call setTlsSctData before setting length", tlsSctDataSet);
+        this.tlsSctDataLength = tlsSctDataLength;
+        return this;
+    }
+
+    public TestSessionBuilder setTlsSctDataEmpty() {
+        tlsSctDataSet = true;
+        return this;
+    }
+
+    public byte[] build() {
+        assertTrue("Must set session data", sessionDataSet);
+        assertTrue("Must call addCertificate at least once", certificatesSet);
+
+        ByteBuffer buf = ByteBuffer.allocate(4096);
+        buf.put((byte) (type & 0xFF));
+
+        buf.putInt(sessionDataLength);
+        buf.put(sessionData);
+
+        buf.putInt(certificatesLength);
+        for (int i = 0; i < certificates.size(); i++) {
+            buf.putInt(certificateLengths.get(i));
+            buf.put(certificates.get(i));
+        }
+
+        if (ocspDataSet) {
+            buf.putInt(ocspDatasLength);
+            for (int i = 0; i < ocspDatas.size(); i++) {
+                buf.putInt(ocspDataLengths.get(i));
+                buf.put(ocspDatas.get(i));
+            }
+
+            if (tlsSctDataSet) {
+                buf.putInt(tlsSctDataLength);
+                buf.put(tlsSctData);
+            }
+        } else {
+            assertFalse("If ocspData is not set, then tlsSctData must not be set", tlsSctDataSet);
+        }
+
+        buf.flip();
+        byte[] output = new byte[buf.remaining()];
+        buf.get(output);
+        return output;
+    }
+}

platform/src/test/java/org/conscrypt/TestSessionBuilderTest.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.conscrypt;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class TestSessionBuilderTest {
+    @Test
+    public void buildsValidBasicSession() {
+        assertArrayEquals(new byte[] {0x11, 0x00, 0x00, 0x00, 0x01, 0x22, 0x00, 0x00, 0x00, 0x01,
+                                  0x00, 0x00, 0x00, 0x01, 0x33},
+                new TestSessionBuilder()
+                        .setType(0x11)
+                        .setSessionData(new byte[] {0x22})
+                        .addCertificate(new byte[] {0x33})
+                        .build());
+    }
+
+    @Test
+    public void buildsValidOcspSession() {
+        assertArrayEquals(
+                new byte[] {
+                        0x11, 0x00, 0x00, 0x00, 0x01, 0x22, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+                        0x00, 0x01, 0x33, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x44,
+                },
+                new TestSessionBuilder()
+                        .setType(0x11)
+                        .setSessionData(new byte[] {0x22})
+                        .addCertificate(new byte[] {0x33})
+                        .addOcspData(new byte[] {0x44})
+                        .build());
+    }
+
+    @Test
+    public void buildsValidOcspAndTlsSctSession() {
+        assertArrayEquals(
+                new byte[] {
+                        0x11, 0x00, 0x00, 0x00, 0x01, 0x22, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+                        0x00, 0x01, 0x33, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x44,
+                        0x00, 0x00, 0x00, 0x01, 0x55,
+                },
+                new TestSessionBuilder()
+                        .setType(0x11)
+                        .setSessionData(new byte[] {0x22})
+                        .addCertificate(new byte[] {0x33})
+                        .addOcspData(new byte[] {0x44})
+                        .setTlsSctData(new byte[] {0x55})
+                        .build());
+    }
+
+    @Test
+    public void buildsValidButEmptyOcspAndTlsSctSession() {
+        assertArrayEquals(
+                new byte[] {
+                        0x11, 0x00, 0x00, 0x00, 0x01, 0x22, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+                        0x00, 0x01, 0x33, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+                        0x00, 0x00, 0x00,
+                },
+                new TestSessionBuilder()
+                        .setType(0x11)
+                        .setSessionData(new byte[] {0x22})
+                        .addCertificate(new byte[] {0x33})
+                        .setOcspDataEmpty()
+                        .setTlsSctDataEmpty()
+                        .build());
+    }
+    @Test
+    public void buildsInvalidOcspAndTlsSctSession() {
+        assertArrayEquals(
+                new byte[] {
+                        0x11, 0x00, 0x33, 0x22, 0x11, 0x22, 0x12, 0x11, 0x22, 0x34, 0x10, 0x20,
+                        0x30, 0x40, 0x33, 0x38, 0x48, 0x18, 0x28, 0x13, 0x24, 0x57, 0x68, 0x44,
+                        (byte) 0x99, (byte) 0x88, 0x77, 0x66, 0x55,
+                },
+                new TestSessionBuilder()
+                        .setType(0x11)
+                        .setSessionData(new byte[] {0x22})
+                        .setSessionDataLength(0x332211)
+                        .addCertificate(new byte[] {0x33})
+                        .setCertificatesLength(0x12112234)
+                        .setCertificateLength(0, 0x10203040)
+                        .addOcspData(new byte[] {0x44})
+                        .setOcspDatasLength(0x38481828)
+                        .setOcspDataLength(0, 0x13245768)
+                        .setTlsSctData(new byte[] {0x55})
+                        .setTlsSctDataLength(0x99887766)
+                        .build());
+    }
+}

commit 3136f7a64424be571927bad89686be85b31432f3

Regression test for SSLSession <-> byte[] serialization/deserialization
Commit 1fa6994406158e6391b5d8b576a6a8aa5a52d13d broke SSLSession
deserialization because it read a byte value for the SSLSession type
when it should have read an int32 value. This was not discovered by
tests because the tests were using the helper class
TestSessionBuilder, which duplicated rather than exercising the
corresponding logic.

Commit d4b9106dd7882398cc8e112b913571ffc18cd05f fixed the
deserialization and made TestSessionBuilder consistent with the code
under test, but didn't add regression coverage for that code under test
that was being fixed.

This CL adds such test coverage by checking that in the round trip:

(1.)      (2.)          (3.)      (4.)          (5.)
byte[] -> SSLSession -> byte[] -> SSLSession -> byte[]

the SSLSession at step (2.) is equal to that at step (4.).
the byte[] at step (3.) is equal to that at step (5.).

The byte[] at step (1.) could be any of three supported types
(OPEN_SSL, OPEN_SSL_WITH_OCSP, OPEN_SSL_WITH_TLS_SCT) whereas
AbstractSessionContext only produces byte[] representations of
type OPEN_SSL_WITH_TLS_SCT (3). This means that conversion must
result in the representation (and int32 value of 0) of missing
(for conversion from type 1) OSCP or (for conversion from type
1 or 2) TLS SCT data being appended. This conversion behavior
is also asserted by the new test.

Since SSLSession.equals() implements Object rather than value
equality semantics, SSLSession attributes are compared via
public getter methods from the SSLSession base class; no attempt
is made to compare the additional attributed introduced by
AbstractOpenSSLSession.

openjdk/src/test/java/org/conscrypt/AbstractSessionContextTest.java
@@ -19,7 +19,13 @@ package org.conscrypt;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import javax.net.ssl.SSLSession;
 import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -562,5 +568,98 @@ public class AbstractSessionContextTest {
     @Test
     public void toSession_Type3TrailingData_Failure() throws Exception {
         assertTrailingDataFails(getType3().build());
+    }
+
+    @Test
+    public void test_reserializableFromByteArray_roundTrip_type1() throws Exception {
+        // Converting OPEN_SSL (type 1) -> OPEN_SSL_WITH_TLS_SCT (type 3) adds
+        // eight zero-bytes:
+        //  1.) 4 bytes for int32 value 0 == countOcspResponses
+        //  2.) 4 bytes for int32 value 0 == tlsSctDataLength
+        // since OPEN_SSL (type 1) cannot contain OSCP or TLS SCT data.
+        check_reserializableFromByteArray_roundTrip(getType1().build(), new byte[8]);
+    }
+
+    @Test
+    public void test_reserializableFromByteArray_roundTrip_type2() throws Exception {
+        // Converting OPEN_SSL_WITH_OCSP (type 2) -> OPEN_SSL_WITH_TLS_SCT (type 3) adds
+        // four zero-bytes for int32 value 0 == tlsSctDataLength
+        // since OPEN_SSL_WITH_OCSP (type 2) cannot contain TLS SCT data.
+        check_reserializableFromByteArray_roundTrip(getType2().build(), new byte[4]);
+    }
+
+    @Test
+    public void test_reserializableFromByteArray_roundTrip_type3() throws Exception {
+        check_reserializableFromByteArray_roundTrip(getType3().build(), new byte[0]);
+    }
+
+    private static void check_reserializableFromByteArray_roundTrip(
+            byte[] data, byte[] expectedTrailingBytesAfterReserialization) throws Exception {
+        SSLSession session = clientCtx.toSession(data, "www.example.com", 12345);
+        byte[] sessionBytes = clientCtx.toBytes(session);
+
+        SSLSession session2 = clientCtx.toSession(sessionBytes, "www.example.com", 12345);
+        byte[] sessionBytes2 = clientCtx.toBytes(session);
+
+        assertSSLSessionEquals(session, session2);
+        assertByteArrayEquals(sessionBytes, sessionBytes2);
+
+        assertEquals("www.example.com", session.getPeerHost());
+        assertEquals(12345, session.getPeerPort());
+        assertTrue(sessionBytes.length >= data.length);
+
+        byte[] expectedReserializedData = concat(data, expectedTrailingBytesAfterReserialization);
+        // AbstractSessionContext.toBytes() always writes type 3 == OPEN_SSL_WITH_TLS_SCT
+        expectedReserializedData[3] = 3;
+        assertByteArrayEquals(expectedReserializedData, sessionBytes);
+    }
+
+    private static byte[] concat(byte[] a, byte[] b) {
+        byte[] result = new byte[a.length + b.length];
+        System.arraycopy(a, 0, result, 0, a.length);
+        System.arraycopy(b, 0, result, a.length, b.length);
+        return result;
+    }
+
+    private static void assertSSLSessionEquals(SSLSession a, SSLSession b) throws Exception {
+        assertEquals(a.getApplicationBufferSize(), b.getApplicationBufferSize());
+        assertEquals(a.getCipherSuite(), b.getCipherSuite());
+        assertEquals(a.getCreationTime(), b.getCreationTime());
+        assertByteArrayEquals(a.getId(), b.getId());
+        assertEquals(a.getLastAccessedTime(), b.getLastAccessedTime());
+        assertArrayEquals(a.getLocalCertificates(), b.getLocalCertificates());
+        assertEquals(a.getLocalPrincipal(), b.getLocalPrincipal());
+        assertArrayEquals(a.getPeerCertificateChain(), b.getPeerCertificateChain());
+        assertArrayEquals(a.getPeerCertificates(), b.getPeerCertificates());
+        assertEquals(a.getPeerHost(), b.getPeerHost());
+        assertEquals(a.getPeerPort(), b.getPeerPort());
+        assertEquals(a.getPeerPrincipal(), b.getPeerPrincipal());
+        assertEquals(a.getProtocol(), b.getProtocol());
+        assertEquals(getValueMap(a), getValueMap(b));
+        assertEquals(a.isValid(), b.isValid());
+
+        assertEquals(a.getClass(), b.getClass());
+
+        // Could potentially cast to AbstractOpenSSLSession here and compare additional fields.
+    }
+
+    private static Map<String, Object> getValueMap(SSLSession sslSession) {
+        Map<String, Object> result = new HashMap<>();
+        for (String valueName : sslSession.getValueNames()) {
+            result.put(valueName, sslSession.getValue(valueName));
+        }
+        return Collections.unmodifiableMap(result);
+    }
+
+    private static <T> void assertArrayEquals(T[] expected, T[] actual) {
+        assertTrue("Expected " + Arrays.toString(expected) + ", got " + Arrays.toString(actual),
+                Arrays.equals(expected, actual));
+    }
+
+    private static void assertByteArrayEquals(byte[] expected, byte[] actual) {
+        // If running on OpenJDK 8+, could use java.util.Base64 for better failure messages:
+        // assertEquals(Base64.encode(expected), Base64.encode(actual));
+        assertTrue("Expected " + Arrays.toString(expected) + ", got " + Arrays.toString(actual),
+                Arrays.equals(expected, actual));
     }
 }