aa914e495cd155321cef1b3c0d888ff34d0292d8
Kenny Root <kroot@google.com>
2017-03-16 17:30:53 -0700

    Add missing @Override annotation
    The original source was missing this annotation, but Error-Prone pointed
    it out.

--- api-doclet/src/main/java/org/conscrypt/doclet/FilterDoclet.java
+++ api-doclet/src/main/java/org/conscrypt/doclet/FilterDoclet.java
@@ -125,6 +125,7 @@ public class FilterDoclet extends com.sun.tools.doclets.standard.Standard {
             this.target = target;
         }
 
+        @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             String methodName = method.getName();
             if (args != null) {
