30004dfac5d97f5c2231440ba95e14ef71d609c2
Nathan Mittler <nathanmittler@google.com>
2017-05-12 10:42:51 -0700

    Refactor OpenSSLSocketImplTest to cover both socket types (#182)

--- common/src/main/java/org/conscrypt/OpenSSLSocketImpl.java
+++ common/src/main/java/org/conscrypt/OpenSSLSocketImpl.java
@@ -438,7 +438,7 @@ public class OpenSSLSocketImpl
                 try {
                     shutdownAndFreeSslNative();
                 } catch (IOException ignored) {
-
+                    // Ignored.
                 }
             }
         }
@@ -702,8 +702,6 @@ public class OpenSSLSocketImpl
          * Reads one byte. If there is no data in the underlying buffer,
          * this operation can block until the data will be
          * available.
-         * @return read value.
-         * @throws IOException
          */
         @Override
         public int read() throws IOException {
@@ -1122,8 +1120,8 @@ public class OpenSSLSocketImpl
     public void close() throws IOException {
         // TODO: Close SSL sockets using a background thread so they close gracefully.
 
-        SSLInputStream sslInputStream = null;
-        SSLOutputStream sslOutputStream = null;
+        SSLInputStream sslInputStream;
+        SSLOutputStream sslOutputStream;
 
         synchronized (stateLock) {
             if (state == STATE_CLOSED) {
