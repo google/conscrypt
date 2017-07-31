9d26fbec9fe1c47d3c4eb4833d1863e60a8241af
Adam Vartanian <flooey@google.com>
2017-07-31 14:59:20 +0100

    Implement Caliper benchmarks for running on Android devices (#10).
    Netty tcnative doesn't work on ARM, so we split out the Netty-dependent
    benchmark portions into benchmark-openjdk and put non-Netty versions
    in benchmark-android.
    
    The gradle operations required to execute the benchmarks on devices are ugly,
    but they appear to work.  This might get better some time in the future
    with improved Caliper support for Android.
    
    This only includes the time-based benchmarks, not the throughput-based ones,
    since those will require some more modifications to work in Caliper and these
    at least get us started.

---
+++ benchmark-caliper/src/main/java/org/conscrypt/CaliperEngineHandshakeBenchmark.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright 2017 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.conscrypt;
+
+import com.google.caliper.BeforeExperiment;
+import com.google.caliper.Param;
+import org.conscrypt.EngineHandshakeBenchmark.Config;
+
+import javax.net.ssl.SSLException;
+
+/**
+ * Benchmark comparing handshake performance of various engine implementations.
+ */
+public class CaliperEngineHandshakeBenchmark {
+    private final CaliperConfig config = new CaliperConfig();
+
+    @Param({TestUtils.TEST_CIPHER})
+    public String a_cipher;
+
+    @Param
+    public BufferType b_buffer;
+
+    @Param
+    public EngineType c_engine;
+
+    private EngineHandshakeBenchmark benchmark;
+
+    @BeforeExperiment
+    public void setUp() throws Exception {
+        benchmark = new EngineHandshakeBenchmark(config);
+    }
+
+    public void timeHandshake(int reps) throws Exception {
+        for (int i = 0; i < reps; ++i) {
+            benchmark.handshake();
+        }
+    }
+
+    private final class CaliperConfig implements Config {
+        @Override
+        public BufferType bufferType() {
+            return b_buffer;
+        }
+
+        @Override
+        public EngineType engineType() {
+            return c_engine;
+        }
+
+        @Override
+        public String cipher() {
+            return a_cipher;
+        }
+
+        @Override
+        public boolean useAlpn() {
+            return false;
+        }
+    }
+}
