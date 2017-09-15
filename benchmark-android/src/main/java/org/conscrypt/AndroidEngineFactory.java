google/conscrypt

commit 047b527e07329fdeb4fd50fd32a6a066c474b7c8

Support renegotiation with sockets (#321)
Fixes #228
Fixes #310

benchmark-android/src/main/java/org/conscrypt/AndroidEngineFactory.java
@@ -15,7 +15,6 @@
  */
 package org.conscrypt;
 
-import static org.conscrypt.TestUtils.PROTOCOL_TLS_V1_2;
 import static org.conscrypt.TestUtils.initEngine;
 
 import java.security.NoSuchAlgorithmException;
@@ -51,7 +50,7 @@ public enum AndroidEngineFactory implements EngineFactory {
 
         private SSLContext newContext() {
             try {
-                return SSLContext.getInstance(PROTOCOL_TLS_V1_2, new OpenSSLProvider());
+                return SSLContext.getInstance(TestUtils.getProtocols()[0], new OpenSSLProvider());
             } catch (NoSuchAlgorithmException e) {
                 throw new RuntimeException(e);
             }

benchmark-jmh/src/jmh/java/org/conscrypt/OpenJdkEngineFactory.java
@@ -17,7 +17,6 @@ package org.conscrypt;
 
 import static io.netty.handler.ssl.SslProvider.OPENSSL;
 import static io.netty.handler.ssl.SslProvider.OPENSSL_REFCNT;
-import static org.conscrypt.TestUtils.PROTOCOL_TLS_V1_2;
 import static org.conscrypt.TestUtils.initClientSslContext;
 import static org.conscrypt.TestUtils.initEngine;
 import static org.conscrypt.TestUtils.initServerSslContext;
@@ -66,7 +65,7 @@ public enum OpenJdkEngineFactory implements EngineFactory {
 
         private SSLContext newContext() {
             try {
-                return SSLContext.getInstance(PROTOCOL_TLS_V1_2);
+                return SSLContext.getInstance(PROTOCOL);
             } catch (NoSuchAlgorithmException e) {
                 throw new RuntimeException(e);
             }
@@ -98,7 +97,7 @@ public enum OpenJdkEngineFactory implements EngineFactory {
 
         private SSLContext newContext() {
             try {
-                return SSLContext.getInstance(PROTOCOL_TLS_V1_2, new OpenSSLProvider());
+                return SSLContext.getInstance(PROTOCOL, new OpenSSLProvider());
             } catch (NoSuchAlgorithmException e) {
                 throw new RuntimeException(e);
             }
@@ -132,7 +131,7 @@ public enum OpenJdkEngineFactory implements EngineFactory {
 
         private SSLContext newContext() {
             try {
-                return SSLContext.getInstance(PROTOCOL_TLS_V1_2, new OpenSSLProvider());
+                return SSLContext.getInstance(PROTOCOL, new OpenSSLProvider());
             } catch (NoSuchAlgorithmException e) {
                 throw new RuntimeException(e);
             }
@@ -255,4 +254,5 @@ public enum OpenJdkEngineFactory implements EngineFactory {
     private static final ApplicationProtocolConfig NETTY_ALPN_CONFIG =
             new ApplicationProtocolConfig(Protocol.ALPN, SelectorFailureBehavior.NO_ADVERTISE,
                     SelectedListenerFailureBehavior.ACCEPT, ApplicationProtocolNames.HTTP_2);
+    private static final String PROTOCOL = TestUtils.getProtocols()[0];
 }

testing/src/main/java/org/conscrypt/TestUtils.java
@@ -31,6 +31,12 @@ import java.nio.charset.Charset;
 import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
 import java.security.Security;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLEngineResult;
@@ -48,29 +54,34 @@ import org.junit.Assume;
  */
 public final class TestUtils {
     public static final Charset UTF_8 = Charset.forName("UTF-8");
-
+    private static final String PROTOCOL_TLS_V1_2 = "TLSv1.2";
+    private static final String PROTOCOL_TLS_V1 = "TLSv1";
+    private static final String[] DESIRED_PROTOCOLS =
+        new String[] {PROTOCOL_TLS_V1_2, /* For Java 6 */ PROTOCOL_TLS_V1};
     private static final Provider JDK_PROVIDER = getDefaultTlsProvider();
     private static final byte[] CHARS =
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".getBytes(UTF_8);
     private static final ByteBuffer EMPTY_BUFFER = ByteBuffer.allocateDirect(0);
-
-    public static final String PROTOCOL_TLS_V1_2 = "TLSv1.2";
-    public static final String PROVIDER_PROPERTY = "SSLContext.TLSv1.2";
-    public static final String LOCALHOST = "localhost";
+    private static final String[] PROTOCOLS = getProtocolsInternal();
 
     static final String TEST_CIPHER = "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256";
 
     private TestUtils() {}
 
     private static Provider getDefaultTlsProvider() {
-        for (Provider p : Security.getProviders()) {
-            if (p.get(PROVIDER_PROPERTY) != null) {
-                return p;
+        for (String protocol : DESIRED_PROTOCOLS) {
+            for (Provider p : Security.getProviders()) {
+                if (hasProtocol(p, protocol)) {
+                    return p;
+                }
             }
         }
-        //throw new RuntimeException("Unable to find a default provider for " + PROVIDER_PROPERTY);
-        // For Java 1.6
+        // For Java 1.6 testing
         return new BouncyCastleProvider();
+    }
+
+    private static boolean hasProtocol(Provider p, String protocol) {
+        return p.get("SSLContext." + protocol) != null;
     }
 
     static Provider getJdkProvider() {
@@ -117,9 +128,7 @@ public final class TestUtils {
 
     public static Provider getConscryptProvider() {
         try {
-            return (Provider) conscryptClass("OpenSSLProvider")
-                .getConstructor()
-                .newInstance();
+            return (Provider) conscryptClass("OpenSSLProvider").getConstructor().newInstance();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -131,7 +140,6 @@ public final class TestUtils {
             Provider[] providers = Security.getProviders();
             if (providers.length == 0 || !providers[0].equals(conscryptProvider)) {
                 Security.insertProviderAt(conscryptProvider, 1);
-                return;
             }
         }
     }
@@ -153,7 +161,7 @@ public final class TestUtils {
      */
     public static Class<?> conscryptClass(String simpleName) throws ClassNotFoundException {
         ClassNotFoundException ex = null;
-        for (String packageName : new String[]{"org.conscrypt", "com.android.org.conscrypt"}) {
+        for (String packageName : new String[] {"org.conscrypt", "com.android.org.conscrypt"}) {
             String name = packageName + "." + simpleName;
             try {
                 return Class.forName(name);
@@ -168,7 +176,17 @@ public final class TestUtils {
      * Returns an array containing only {@link #PROTOCOL_TLS_V1_2}.
      */
     public static String[] getProtocols() {
-        return new String[] {PROTOCOL_TLS_V1_2};
+        return PROTOCOLS;
+    }
+
+    private static String[] getProtocolsInternal() {
+        List<String> protocols = new ArrayList<String>();
+        for (String protocol : DESIRED_PROTOCOLS) {
+            if (hasProtocol(getJdkProvider(), protocol)) {
+                protocols.add(protocol);
+            }
+        }
+        return protocols.toArray(new String[protocols.size()]);
     }
 
     public static SSLSocketFactory getJdkSocketFactory() {
@@ -223,11 +241,40 @@ public final class TestUtils {
         return serverContext.getServerSocketFactory();
     }
 
-    private static SSLContext newContext(Provider provider) {
+    static SSLContext newContext(Provider provider) {
         try {
             return SSLContext.getInstance("TLS", provider);
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    static String[] getCommonCipherSuites() {
+        SSLContext jdkContext =
+                TestUtils.initSslContext(newContext(getJdkProvider()), TestKeyStore.getClient());
+        SSLContext conscryptContext = TestUtils.initSslContext(
+                newContext(getConscryptProvider()), TestKeyStore.getClient());
+        Set<String> supported = new LinkedHashSet<String>();
+        supported.addAll(supportedCiphers(jdkContext));
+        supported.retainAll(supportedCiphers(conscryptContext));
+        filterCiphers(supported);
+
+        return supported.toArray(new String[supported.size()]);
+    }
+
+    private static List<String> supportedCiphers(SSLContext ctx) {
+        return Arrays.asList(ctx.getDefaultSSLParameters().getCipherSuites());
+    }
+
+    private static void filterCiphers(Iterable<String> ciphers) {
+        // Filter all non-TLS ciphers.
+        Iterator<String> iter = ciphers.iterator();
+        while (iter.hasNext()) {
+            String cipher = iter.next();
+            if (cipher.startsWith("SSL_") || cipher.startsWith("TLS_EMPTY")
+                    || cipher.contains("_RC4_")) {
+                iter.remove();
+            }
         }
     }
 
@@ -312,10 +359,12 @@ public final class TestUtils {
      * Performs the intial TLS handshake between the two {@link SSLEngine} instances.
      */
     public static void doEngineHandshake(SSLEngine clientEngine, SSLEngine serverEngine,
-            ByteBuffer clientAppBuffer, ByteBuffer clientPacketBuffer, ByteBuffer serverAppBuffer,
-            ByteBuffer serverPacketBuffer) throws SSLException {
-        clientEngine.beginHandshake();
-        serverEngine.beginHandshake();
+        ByteBuffer clientAppBuffer, ByteBuffer clientPacketBuffer, ByteBuffer serverAppBuffer,
+        ByteBuffer serverPacketBuffer, boolean beginHandshake) throws SSLException {
+        if (beginHandshake) {
+            clientEngine.beginHandshake();
+            serverEngine.beginHandshake();
+        }
 
         SSLEngineResult clientResult;
         SSLEngineResult serverResult;
@@ -366,9 +415,9 @@ public final class TestUtils {
             assertEquals(serverPacketBuffer.position() - sTOcPos, clientResult.bytesConsumed());
             assertEquals(clientPacketBuffer.position() - cTOsPos, serverResult.bytesConsumed());
             assertEquals(clientAppBuffer.position() - clientAppReadBufferPos,
-                    clientResult.bytesProduced());
+                clientResult.bytesProduced());
             assertEquals(serverAppBuffer.position() - serverAppReadBufferPos,
-                    serverResult.bytesProduced());
+                serverResult.bytesProduced());
 
             clientPacketBuffer.compact();
             serverPacketBuffer.compact();

benchmark-base/src/main/java/org/conscrypt/EngineHandshakeBenchmark.java
@@ -89,7 +89,7 @@ public final class EngineHandshakeBenchmark {
         serverPacketBuffer.clear();
 
         doEngineHandshake(client, server, clientApplicationBuffer, clientPacketBuffer,
-                serverApplicationBuffer, serverPacketBuffer);
+                serverApplicationBuffer, serverPacketBuffer, true);
         engineFactory.dispose(client);
         engineFactory.dispose(server);
     }

benchmark-base/src/main/java/org/conscrypt/EngineWrapBenchmark.java
@@ -90,7 +90,7 @@ public final class EngineWrapBenchmark {
 
         // Complete the initial TLS handshake.
         doEngineHandshake(clientEngine, serverEngine, clientApplicationBuffer, clientPacketBuffer,
-                serverApplicationBuffer, serverPacketBuffer);
+                serverApplicationBuffer, serverPacketBuffer, true);
 
         // Populate the pre-encrypted buffer for use with the unwrap benchmark.
         preEncryptedBuffer = bufferType.newBuffer(clientEngine.getSession().getPacketBufferSize());

common/src/jni/main/cpp/conscrypt/native_crypto.cc
@@ -5801,12 +5801,12 @@ static ssl_verify_result_t cert_verify_callback(SSL* ssl, CONSCRYPT_UNUSED uint8
  * False Start support, since SSL_do_handshake returns before the handshake is
  * completed in this case.
  */
-static void info_callback(const SSL* ssl, int where, int ret) {
-    JNI_TRACE("ssl=%p info_callback where=0x%x ret=%d", ssl, where, ret);
+static void info_callback(const SSL* ssl, int type, int value) {
+    JNI_TRACE("ssl=%p info_callback type=0x%x value=%d", ssl, type, value);
     if (conscrypt::trace::kWithJniTrace) {
-        info_callback_LOG(ssl, where, ret);
+        info_callback_LOG(ssl, type, value);
     }
-    if (!(where & SSL_CB_HANDSHAKE_DONE) && !(where & SSL_CB_HANDSHAKE_START)) {
+    if (!(type & SSL_CB_HANDSHAKE_DONE) && !(type & SSL_CB_HANDSHAKE_START)) {
         JNI_TRACE("ssl=%p info_callback ignored", ssl);
         return;
     }
@@ -5829,7 +5829,7 @@ static void info_callback(const SSL* ssl, int where, int ret) {
     jmethodID methodID = env->GetMethodID(cls, "onSSLStateChange", "(II)V");
 
     JNI_TRACE("ssl=%p info_callback calling onSSLStateChange", ssl);
-    env->CallVoidMethod(sslHandshakeCallbacks, methodID, where, ret);
+    env->CallVoidMethod(sslHandshakeCallbacks, methodID, type, value);
 
     if (env->ExceptionCheck()) {
         JNI_TRACE("ssl=%p info_callback exception", ssl);

common/src/main/java/org/conscrypt/ActiveSession.java
@@ -41,7 +41,6 @@ final class ActiveSession implements SSLSession {
     private AbstractSessionContext sessionContext;
     private byte[] id;
     private long creationTime;
-    private String cipherSuite;
     private String protocol;
     private String peerHost;
     private int peerPort = -1;
@@ -274,12 +273,11 @@ final class ActiveSession implements SSLSession {
 
     @Override
     public String getCipherSuite() {
-        if (cipherSuite == null) {
-            synchronized (ssl) {
-                cipherSuite = ssl.getCipherSuite();
-            }
+        // Always get the Cipher from the SSL directly since it may have changed during a
+        // renegotiation.
+        synchronized (ssl) {
+            return ssl.getCipherSuite();
         }
-        return cipherSuite;
     }
 
     @Override

common/src/main/java/org/conscrypt/ConscryptEngine.java
@@ -363,18 +363,20 @@ final class ConscryptEngine extends SSLEngine implements NativeCrypto.SSLHandsha
 
     private void beginHandshakeInternal() throws SSLException {
         switch (state) {
-            case STATE_MODE_SET:
+            case STATE_NEW: {
+                throw new IllegalStateException("Client/server mode must be set before handshake");
+            }
+            case STATE_MODE_SET: {
                 // We know what mode to handshake in but have not started the handshake, proceed
                 break;
-            case STATE_HANDSHAKE_STARTED:
-                // We've already started the handshake, just return
-                return;
+            }
             case STATE_CLOSED_INBOUND:
             case STATE_CLOSED_OUTBOUND:
             case STATE_CLOSED:
                 throw new IllegalStateException("Engine has already been closed");
             default:
-                throw new IllegalStateException("Client/server mode must be set before handshake");
+                // We've already started the handshake, just return
+                return;
         }
 
         state = STATE_HANDSHAKE_STARTED;
@@ -1522,6 +1524,7 @@ final class ConscryptEngine extends SSLEngine implements NativeCrypto.SSLHandsha
                     // For clients, this will allow the NEED_UNWRAP status to be
                     // returned.
                     state = STATE_HANDSHAKE_STARTED;
+                    handshakeFinished = false;
                     break;
                 }
                 case SSL_CB_HANDSHAKE_DONE: {

commit 74ae419a0c8a3289d9af7e85e0354ab905f45fbb

Fix benchmarks broken by #321
The cipher used in the benchmarks requires TLS 1.2.

benchmark-android/src/main/java/org/conscrypt/AndroidEngineFactory.java
@@ -15,8 +15,6 @@
  */
 package org.conscrypt;
 
-import static org.conscrypt.TestUtils.initEngine;
-
 import java.security.NoSuchAlgorithmException;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
@@ -71,5 +69,12 @@ public enum AndroidEngineFactory implements EngineFactory {
 
     private static SSLContext newConscryptServerContext() {
         return TestUtils.newServerSslContext(TestUtils.getConscryptProvider());
+    }
+
+    static SSLEngine initEngine(SSLEngine engine, String cipher, boolean client) {
+        engine.setEnabledProtocols(new String[]{"TLSv1.2"});
+        engine.setEnabledCipherSuites(new String[] {cipher});
+        engine.setUseClientMode(client);
+        return engine;
     }
 }

benchmark-jmh/src/jmh/java/org/conscrypt/JmhEngineWrapBenchmark.java
@@ -59,7 +59,7 @@ public class JmhEngineWrapBenchmark {
     @Param
     public BufferType b_buffer;
 
-    @Param({"64", "512", "4096"})
+    @Param({"128", "4096"})
     public int c_message;
 
     @Param

benchmark-jmh/src/jmh/java/org/conscrypt/OpenJdkEngineFactory.java
@@ -18,7 +18,6 @@ package org.conscrypt;
 import static io.netty.handler.ssl.SslProvider.OPENSSL;
 import static io.netty.handler.ssl.SslProvider.OPENSSL_REFCNT;
 import static org.conscrypt.TestUtils.initClientSslContext;
-import static org.conscrypt.TestUtils.initEngine;
 import static org.conscrypt.TestUtils.initServerSslContext;
 
 import io.netty.buffer.PooledByteBufAllocator;
@@ -42,7 +41,7 @@ final class OpenJdkEngineFactoryConfig {
     static final ApplicationProtocolConfig NETTY_ALPN_CONFIG =
             new ApplicationProtocolConfig(Protocol.ALPN, SelectorFailureBehavior.NO_ADVERTISE,
                     SelectedListenerFailureBehavior.ACCEPT, ApplicationProtocolNames.HTTP_2);
-    static final String PROTOCOL = TestUtils.getProtocols()[0];
+    static final String PROTOCOL = "TLSv1.2";
 }
 
 /**
@@ -254,5 +253,12 @@ public enum OpenJdkEngineFactory implements EngineFactory {
         } catch (SSLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    static SSLEngine initEngine(SSLEngine engine, String cipher, boolean client) {
+        engine.setEnabledProtocols(new String[]{OpenJdkEngineFactoryConfig.PROTOCOL});
+        engine.setEnabledCipherSuites(new String[] {cipher});
+        engine.setUseClientMode(client);
+        return engine;
     }
 }

testing/src/main/java/org/conscrypt/TestUtils.java
@@ -309,16 +309,6 @@ public final class TestUtils {
         return msg;
     }
 
-    /**
-     * Initializes the given engine with the cipher and client mode.
-     */
-    static SSLEngine initEngine(SSLEngine engine, String cipher, boolean client) {
-        engine.setEnabledProtocols(getProtocols());
-        engine.setEnabledCipherSuites(new String[] {cipher});
-        engine.setUseClientMode(client);
-        return engine;
-    }
-
     static SSLContext newClientSslContext(Provider provider) {
         SSLContext context = newContext(provider);
         return initClientSslContext(context);