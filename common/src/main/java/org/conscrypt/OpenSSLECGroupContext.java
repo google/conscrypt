3ed39e4bd63fd38c9c173d533225051e4dad0ba7
Adam Vartanian <flooey@google.com>
2017-11-10 13:11:56 +0000

    Remove the leading zero in secp521r1 detection value.
    BigInteger.toString() doesn't produce leading zeroes, so this was
    preventing us from detecting secp521r1 when specified as parameters
    instead of by name.

--- common/src/main/java/org/conscrypt/OpenSSLECGroupContext.java
+++ common/src/main/java/org/conscrypt/OpenSSLECGroupContext.java
@@ -119,7 +119,7 @@ final class OpenSSLECGroupContext {
                 break;
             case 521:
                 if (p.toString(16).equals("1ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff") &&
-                    b.toString(16).equals("051953eb9618e1c9a1f929a21a0b68540eea2da725b99b315f3b8b489918ef109e156193951ec7e937b1652c0bd3bb1bf073573df883d2c34f1ef451fd46b503f00") &&
+                    b.toString(16).equals("51953eb9618e1c9a1f929a21a0b68540eea2da725b99b315f3b8b489918ef109e156193951ec7e937b1652c0bd3bb1bf073573df883d2c34f1ef451fd46b503f00") &&
                     x.toString(16).equals("c6858e06b70404e9cd9e3ecb662395b4429c648139053fb521f828af606b4d3dbaa14b5e77efe75928fe1dc127a2ffa8de3348b3c1856a429bf97e7e31c2e5bd66") &&
                     y.toString(16).equals("11839296a789a3bc0045c8a5fb42c7d1bd998f54449579b446817afbd17273e662c97ee72995ef42640c550b9013fad0761353c7086a272c24088be94769fd16650")) {
                     curveName = "secp521r1";
