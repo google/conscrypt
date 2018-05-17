b0993c91ca4369cab24e172eb0298e8cd332bc12
Aemon Cannon <aemoncannon@gmail.com>
2018-05-17 13:12:50 -0500

    Don't load libgmscore in NativeCryptoJni (#492)
    This load is no longer necessary in GMS Core.

--- android/src/main/java/org/conscrypt/NativeCryptoJni.java
+++ android/src/main/java/org/conscrypt/NativeCryptoJni.java
@@ -23,7 +23,6 @@ package org.conscrypt;
 class NativeCryptoJni {
     public static void init() {
         if ("com.google.android.gms.org.conscrypt".equals(NativeCrypto.class.getPackage().getName())) {
-            System.loadLibrary("gmscore");
             System.loadLibrary("conscrypt_gmscore_jni");
         } else {
             System.loadLibrary("conscrypt_jni");
