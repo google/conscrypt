ffba95f812d11cb7e27a884f40ef251553115adc
Pete Bentley <prb@google.com>
2019-11-07 15:31:50 +0000

    Handle removal of a handshake listener during event delivery.
    Fixes #783

--- common/src/test/java/org/conscrypt/javax/net/ssl/SSLSocketVersionCompatibilityTest.java
+++ common/src/test/java/org/conscrypt/javax/net/ssl/SSLSocketVersionCompatibilityTest.java
@@ -417,6 +417,7 @@ public class SSLSocketVersionCompatibilityTest {
                         handshakeCompletedListenerCalled.notify();
                     }
                     handshakeCompletedListenerCalled[0] = true;
+                    socket.removeHandshakeCompletedListener(this);
                 } catch (RuntimeException e) {
                     throw e;
                 } catch (Exception e) {
