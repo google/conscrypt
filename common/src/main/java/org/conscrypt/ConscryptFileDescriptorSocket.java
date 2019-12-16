eec9a040f1af886d66cb8d01805514c87788af69
Pete Bentley <prb@google.com>
2019-12-16 10:37:58 +0000

    Comment the provide*Handshake* methods for clarity.

--- common/src/main/java/org/conscrypt/ConscryptFileDescriptorSocket.java
+++ common/src/main/java/org/conscrypt/ConscryptFileDescriptorSocket.java
@@ -691,12 +691,15 @@ class ConscryptFileDescriptorSocket extends OpenSSLSocketImpl
         return activeSession;
     }
 
+    // After handshake has started, provide active session otherwise a null session,
+    // for code which needs to read session attributes without triggering the handshake.
     private ConscryptSession provideAfterHandshakeSession() {
         return (state < STATE_HANDSHAKE_STARTED)
             ? SSLNullSession.getNullSession()
             : provideSession();
     }
 
+    // If handshake is in progress, provide active session otherwise a null session.
     private ConscryptSession provideHandshakeSession() {
         synchronized (ssl) {
             return state >= STATE_HANDSHAKE_STARTED && state < STATE_READY ? activeSession
