27bf3ae20fd717aa666fa4811b97510c71be0cfa
Almaz Mingaleev <mingaleev@google.com>
2020-11-05 19:06:01 +0000

    Flip isAssignableFrom checks in OpenSSLECKeyFactory and OpenSSLRSAKeyFactory

--- common/src/test/java/org/conscrypt/java/security/KeyFactoryTestEC.java
+++ common/src/test/java/org/conscrypt/java/security/KeyFactoryTestEC.java
@@ -15,54 +15,141 @@
  */
 package org.conscrypt.java.security;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.math.BigInteger;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.Security;
 import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECParameterSpec;
+import java.security.spec.ECPoint;
 import java.security.spec.ECPrivateKeySpec;
 import java.security.spec.ECPublicKeySpec;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import tests.util.ServiceTester;
 
 @RunWith(JUnit4.class)
-public class KeyFactoryTestEC extends
-    AbstractKeyFactoryTest<ECPublicKeySpec, ECPrivateKeySpec> {
+public class KeyFactoryTestEC extends AbstractKeyFactoryTest<ECPublicKeySpec, ECPrivateKeySpec> {
+    public KeyFactoryTestEC() {
+        super("EC", ECPublicKeySpec.class, ECPrivateKeySpec.class);
+    }
 
-  public KeyFactoryTestEC() {
-    super("EC", ECPublicKeySpec.class, ECPrivateKeySpec.class);
-  }
+    @Override
+    public ServiceTester customizeTester(ServiceTester tester) {
+        // BC's EC keys always use explicit params, even though it's a bad idea, and we don't
+        // support those, so don't test BC keys
+        return tester.skipProvider("BC");
+    }
 
-  @Override
-  public ServiceTester customizeTester(ServiceTester tester) {
-    // BC's EC keys always use explicit params, even though it's a bad idea, and we don't support
-    // those, so don't test BC keys
-    return tester.skipProvider("BC");
-  }
+    @Override
+    protected void check(KeyPair keyPair) throws Exception {
+        new SignatureHelper("SHA256withECDSA").test(keyPair);
+    }
 
-  @Override
-  protected void check(KeyPair keyPair) throws Exception {
-    new SignatureHelper("SHA256withECDSA").test(keyPair);
-  }
+    @Override
+    protected List<KeyPair> getKeys() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        return Arrays.asList(
+                new KeyPair(DefaultKeys.getPublicKey("EC"), DefaultKeys.getPrivateKey("EC")),
+                new KeyPair(new TestPublicKey(DefaultKeys.getPublicKey("EC")),
+                        new TestPrivateKey(DefaultKeys.getPrivateKey("EC"))),
+                new KeyPair(new TestECPublicKey((ECPublicKey) DefaultKeys.getPublicKey("EC")),
+                        new TestECPrivateKey((ECPrivateKey) DefaultKeys.getPrivateKey("EC"))));
+    }
 
-  @Override
-  protected List<KeyPair> getKeys() throws NoSuchAlgorithmException, InvalidKeySpecException {
-    return Arrays.asList(
-        new KeyPair(
-            DefaultKeys.getPublicKey("EC"),
-            DefaultKeys.getPrivateKey("EC")
-        ),
-        new KeyPair(
-            new TestPublicKey(DefaultKeys.getPublicKey("EC")),
-            new TestPrivateKey(DefaultKeys.getPrivateKey("EC"))
-        ),
-        new KeyPair(
-            new TestECPublicKey((ECPublicKey)DefaultKeys.getPublicKey("EC")),
-            new TestECPrivateKey((ECPrivateKey)DefaultKeys.getPrivateKey("EC"))
-        )
-    );
-  }
+    @Test
+    public void shouldThrowInvalidKeySpecException_whenKeySpecIsOdd() throws Exception {
+        Provider p = Security.getProvider(StandardNames.JSSE_PROVIDER_NAME);
+        final KeyFactory factory = KeyFactory.getInstance("EC", p);
+
+        try {
+            assertThat(factory.getKeySpec(
+                               new TestECPublicKey((ECPublicKey) DefaultKeys.getPublicKey("EC")),
+                               FakeECPublicKeySpec.class),
+                    instanceOf(FakeECPublicKeySpec.class));
+            fail();
+        } catch (InvalidKeySpecException e) {
+            // expected
+        }
+
+        try {
+            assertThat(
+                    factory.getKeySpec(DefaultKeys.getPublicKey("EC"), FakeECPublicKeySpec.class),
+                    instanceOf(FakeECPublicKeySpec.class));
+            fail();
+        } catch (InvalidKeySpecException e) {
+            // expected
+        }
+
+        try {
+            assertThat(factory.getKeySpec(
+                               new TestECPrivateKey((ECPrivateKey) DefaultKeys.getPrivateKey("EC")),
+                               FakeECPrivateKeySpec.class),
+                    instanceOf(FakeECPrivateKeySpec.class));
+            fail();
+        } catch (InvalidKeySpecException e) {
+            // expected
+        }
+
+        try {
+            assertThat(
+                    factory.getKeySpec(DefaultKeys.getPrivateKey("EC"), FakeECPrivateKeySpec.class),
+                    instanceOf(FakeECPrivateKeySpec.class));
+            fail();
+        } catch (InvalidKeySpecException e) {
+            // expected
+        }
+
+        try {
+            assertThat(factory.getKeySpec(DefaultKeys.getPrivateKey("EC"), FakePKCS8.class),
+                    instanceOf(FakePKCS8.class));
+            fail();
+        } catch (InvalidKeySpecException e) {
+            // expected
+        }
+
+        try {
+            assertThat(factory.getKeySpec(DefaultKeys.getPublicKey("EC"), FakeX509.class),
+                    instanceOf(FakeX509.class));
+            fail();
+        } catch (InvalidKeySpecException e) {
+            // expected
+        }
+    }
+
+    private static class FakeECPublicKeySpec extends ECPublicKeySpec {
+        public FakeECPublicKeySpec(ECPoint w, ECParameterSpec params) {
+            super(w, params);
+        }
+    }
+
+    private static class FakeECPrivateKeySpec extends ECPrivateKeySpec {
+        public FakeECPrivateKeySpec(BigInteger s, ECParameterSpec params) {
+            super(s, params);
+        }
+    }
+
+    private static class FakePKCS8 extends PKCS8EncodedKeySpec {
+        public FakePKCS8(byte[] encodedKey) {
+            super(encodedKey);
+        }
+    }
+
+    private static class FakeX509 extends X509EncodedKeySpec {
+        public FakeX509(byte[] encodedKey) {
+            super(encodedKey);
+        }
+    }
 }
--- common/src/test/java/org/conscrypt/java/security/KeyFactoryTestRSA.java
+++ common/src/test/java/org/conscrypt/java/security/KeyFactoryTestRSA.java
@@ -15,14 +15,19 @@
  */
 package org.conscrypt.java.security;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
+import java.math.BigInteger;
 import java.security.KeyFactory;
 import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.Provider;
 import java.security.PublicKey;
 import java.security.Security;
+import java.security.interfaces.RSAPrivateCrtKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.RSAPrivateCrtKeySpec;
@@ -34,9 +39,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class KeyFactoryTestRSA extends
-        AbstractKeyFactoryTest<RSAPublicKeySpec, RSAPrivateKeySpec> {
-
+public class KeyFactoryTestRSA extends AbstractKeyFactoryTest<RSAPublicKeySpec, RSAPrivateKeySpec> {
     public KeyFactoryTestRSA() {
         super("RSA", RSAPublicKeySpec.class, RSAPrivateKeySpec.class);
     }
@@ -65,13 +68,13 @@ public class KeyFactoryTestRSA extends
     }
 
     @Test
-    public void testInvalidKeySpec() throws Exception {
+    public void shouldThrowInvalidKeySpec_whenKeyIsInvalid() throws Exception {
         Provider p = Security.getProvider(StandardNames.JSSE_PROVIDER_NAME);
         final KeyFactory factory = KeyFactory.getInstance("RSA", p);
 
         try {
             factory.getKeySpec(new TestPrivateKey(DefaultKeys.getPrivateKey("RSA"), "Invalid"),
-                RSAPrivateKeySpec.class);
+                    RSAPrivateKeySpec.class);
             fail();
         } catch (InvalidKeySpecException e) {
             // expected
@@ -79,7 +82,7 @@ public class KeyFactoryTestRSA extends
 
         try {
             factory.getKeySpec(new TestPrivateKey(DefaultKeys.getPrivateKey("RSA"), "Invalid"),
-                RSAPrivateCrtKeySpec.class);
+                    RSAPrivateCrtKeySpec.class);
             fail();
         } catch (InvalidKeySpecException e) {
             // expected
@@ -87,10 +90,93 @@ public class KeyFactoryTestRSA extends
 
         try {
             factory.getKeySpec(new TestPublicKey(DefaultKeys.getPublicKey("RSA"), "Invalid"),
-                RSAPublicKeySpec.class);
+                    RSAPublicKeySpec.class);
             fail();
         } catch (InvalidKeySpecException e) {
             // expected
+        }
+    }
+
+    @Test
+    public void shouldThrowInvalidKeySpec_whenKeySpecIsOdd() throws Exception {
+        Provider p = Security.getProvider(StandardNames.JSSE_PROVIDER_NAME);
+        final KeyFactory factory = KeyFactory.getInstance("RSA", p);
+
+        try {
+            assertThat(
+                    factory.getKeySpec(DefaultKeys.getPublicKey("RSA"), FakeRSAPublicKeySpec.class),
+                    instanceOf(FakeRSAPublicKeySpec.class));
+            fail();
+        } catch (InvalidKeySpecException e) {
+            // expected
+        }
+
+        try {
+            assertThat(factory.getKeySpec(generateRsaKey(), FakeRSAPrivateCrtKeySpec.class),
+                    instanceOf(FakeRSAPrivateCrtKeySpec.class));
+            fail();
+        } catch (InvalidKeySpecException e) {
+            // expected
+        }
+
+        try {
+            assertThat(factory.getKeySpec(
+                               DefaultKeys.getPrivateKey("RSA"), FakeRSAPrivateCrtKeySpec.class),
+                    instanceOf(FakeRSAPrivateCrtKeySpec.class));
+            fail();
+        } catch (InvalidKeySpecException e) {
+            // expected
+        }
+
+        try {
+            assertThat(factory.getKeySpec(DefaultKeys.getPrivateKey("RSA"), FakePKCS8.class),
+                    instanceOf(FakePKCS8.class));
+            fail();
+        } catch (InvalidKeySpecException e) {
+            // expected
+        }
+
+        try {
+            assertThat(factory.getKeySpec(DefaultKeys.getPublicKey("RSA"), FakeX509.class),
+                    instanceOf(FakeX509.class));
+            fail();
+        } catch (InvalidKeySpecException e) {
+            // expected
+        }
+    }
+
+    private static RSAPrivateCrtKey generateRsaKey() throws Exception {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(512);
+
+        KeyPair keyPair = kpg.generateKeyPair();
+        return (RSAPrivateCrtKey) keyPair.getPrivate();
+    }
+
+    private static class FakeRSAPublicKeySpec extends RSAPublicKeySpec {
+        public FakeRSAPublicKeySpec(BigInteger modulus, BigInteger publicExponent) {
+            super(modulus, publicExponent);
+        }
+    }
+
+    private static class FakePKCS8 extends PKCS8EncodedKeySpec {
+        public FakePKCS8(byte[] encodedKey) {
+            super(encodedKey);
+        }
+    }
+
+    private static class FakeRSAPrivateCrtKeySpec extends RSAPrivateCrtKeySpec {
+        public FakeRSAPrivateCrtKeySpec(BigInteger modulus, BigInteger publicExponent,
+                BigInteger privateExponent, BigInteger primeP, BigInteger primeQ,
+                BigInteger primeExponentP, BigInteger primeExponentQ, BigInteger crtCoefficient) {
+            super(modulus, publicExponent, privateExponent, primeP, primeQ, primeExponentP,
+                    primeExponentQ, crtCoefficient);
+        }
+    }
+
+    private static class FakeX509 extends X509EncodedKeySpec {
+        public FakeX509(byte[] encodedKey) {
+            super(encodedKey);
         }
     }
 }
