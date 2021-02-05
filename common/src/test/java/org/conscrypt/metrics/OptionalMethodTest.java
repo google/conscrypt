da1bbee8ebd7aef36a681ad0e58f02ae493edfc9
Pete Bentley <prb@google.com>
2021-02-05 13:32:27 +0000

    Fix import ordering.

--- common/src/test/java/org/conscrypt/metrics/OptionalMethodTest.java
+++ common/src/test/java/org/conscrypt/metrics/OptionalMethodTest.java
@@ -1,12 +1,12 @@
 package org.conscrypt.metrics;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class OptionalMethodTest {
--- common/src/test/java/org/conscrypt/metrics/ProtocolTest.java
+++ common/src/test/java/org/conscrypt/metrics/ProtocolTest.java
@@ -1,10 +1,10 @@
 package org.conscrypt.metrics;
 
+import static org.junit.Assert.assertSame;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import static org.junit.Assert.assertSame;
 
 @RunWith(JUnit4.class)
 public class ProtocolTest {
