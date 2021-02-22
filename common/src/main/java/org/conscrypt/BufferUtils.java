2a542f49afd53d25f2815b3286647a26609633d7
Pete Bentley <44170157+prbprbprb@users.noreply.github.com>
2021-02-22 17:11:36 +0000

    Fix ConscryptEngine.wrap() behaviour when multiple buffers passed in. (#947)
    * Fix ConscryptEngine.wrap() behaviour when multiple buffers passed in.
    
    Fixes #929.
    
    Behaviour unchanged for the common case of passing a single
    buffer to `wrap()`.
    
    When passing an array of small buffers, instead of passing them
    individually to BoringSSL, which generates one TLS record per buffer,
    coalesce theem into a single larger buffer so that a single maximal
    sized TLS record is generated, which was the original intent here.
    
    This is a slight behaviour change for apps which aren't hitting #929
    but I believe based on commentary on the bug this is closer to
    the RI behaviour.
    
    Currently, if an app passes multiple buffers into a single `wrap()`
    call, one TLS record is generated for each which means on the
    receiving end multiple `unwrap()` calls are required so the app has
    to loop, unwrapping until no more records are available.  After
    this change only a single unwrap is required so the same code will
    still work. However if an app is making assumptions about where TLS
    record boundaries lie then it might have issues - such an app is
    buggy as `SSLEngine` makes no guarantees in this area.  This assumption
    was inadvertently baked into a couple of tests  and has been removed now.

---
+++ common/src/main/java/org/conscrypt/BufferUtils.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.conscrypt;
+
+import static java.lang.Math.min;
+import static org.conscrypt.Preconditions.checkArgument;
+
+import java.nio.ByteBuffer;
+
+final class BufferUtils {
+    private BufferUtils() {}
+
+    /**
+     * Throws {@link IllegalArgumentException} if any of the buffers in the array are null.
+     */
+    static void checkNotNull(ByteBuffer[] buffers) {
+        for (ByteBuffer buffer : buffers) {
+            if (buffer == null) {
+                throw new IllegalArgumentException("Null buffer in array");
+            }
+        }
+    }
+
+    /**
+     * Returns the total number of bytes remaining in the buffer array.
+     */
+    static long remaining(ByteBuffer[] buffers) {
+        long size = 0;
+        for (ByteBuffer buffer : buffers) {
+            size += buffer.remaining();
+        }
+        return size;
+    }
+
+    /**
+     * Marks {@code toConsume} bytes of data as consumed from the buffer array.
+     *
+     * @throws IllegalArgumentException if there are fewer than {@code toConsume} bytes remaining
+     */
+    static void consume(ByteBuffer[] sourceBuffers, int toConsume) {
+        for (ByteBuffer sourceBuffer : sourceBuffers) {
+            int amount = min(sourceBuffer.remaining(), toConsume);
+            if (amount > 0) {
+                sourceBuffer.position(sourceBuffer.position() + amount);
+                toConsume -= amount;
+                if (toConsume == 0) {
+                    break;
+                }
+            }
+        }
+        if (toConsume > 0) {
+            throw new IllegalArgumentException("toConsume > data size");
+        }
+    }
+
+    /**
+     * Looks for a buffer in the buffer array which EITHER is larger than {@code minSize} AND
+     * has no preceding non-empty buffers OR is the only non-empty buffer in the array.
+     */
+    static ByteBuffer getBufferLargerThan(ByteBuffer[] buffers, int minSize) {
+        int length = buffers.length;
+        for (int i = 0; i < length; i++) {
+            ByteBuffer buffer = buffers[i];
+            int remaining = buffer.remaining();
+            if (remaining > 0) {
+                if (remaining >= minSize) {
+                    return buffer;
+                }
+                for (int j = i + 1; j < length; j++) {
+                    if (buffers[j].remaining() > 0) {
+                        return null;
+                    }
+                }
+                return buffer;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Copies up to {@code maxAmount} bytes from a buffer array to {@code destination}.
+     * The copied data is <b>not</b> marked as consumed from the source buffers, on the
+     * assumption the copy will be passed to some method which will consume between 0 and
+     * {@code maxAmount} bytes which can then be reflected in the source array using the
+     * {@code consume()} method.
+     *
+     */
+    static ByteBuffer copyNoConsume(ByteBuffer[] buffers, ByteBuffer destination, int maxAmount) {
+	checkArgument(destination.remaining() >= maxAmount, "Destination buffer too small");
+	int needed = maxAmount;
+        for (ByteBuffer buffer : buffers) {
+	    int remaining = buffer.remaining();
+            if (remaining > 0) {
+                // If this buffer can fit completely then copy it all, otherwise temporarily
+                // adjust its limit to fill so as to the output buffer completely
+                int oldPosition = buffer.position();
+                if (remaining <= needed) {
+                    destination.put(buffer);
+                    needed -= remaining;
+                } else {
+                    int oldLimit = buffer.limit();
+                    buffer.limit(buffer.position() + needed);
+                    destination.put(buffer);
+                    buffer.limit(oldLimit);
+                    needed = 0;
+                }
+                // Restore the buffer's position, the data won't get marked as consumed until
+                // outputBuffer has been successfully consumed.
+                buffer.position(oldPosition);
+                if (needed == 0) {
+                    break;
+                }
+            }
+        }
+        destination.flip();
+        return destination;
+    }
+}
---
+++ common/src/test/java/org/conscrypt/BufferUtilsTest.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.conscrypt;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.nio.ByteBuffer;
+import org.conscrypt.TestUtils.BufferType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+
+@RunWith(Parameterized.class)
+public class BufferUtilsTest {
+    private static final int K64 = 64 * 1024;
+    private static final int K16 = 16 * 1024;
+
+    private static final int[][] TEST_SIZES = {
+            // All even numbers as several tests use size/2
+            { 0 },
+            { 0, 0, 0, 0 },
+            { 0, 0, 0, 2 },
+            { 2, 0, 0, 0 },
+            { 100, 200, 300 },
+            { 1000, 2000, 3000 },
+            { K16 },
+            { 0, 0, K16 },
+            { K16, 0, 0 },
+            { K64 },
+            { 0, 0, K64 },
+            { K64, 0, 0 },
+            { 100, 100, K64 },
+            { K64, 100, 100 },
+            { K64, K64, K64 },
+    };
+
+
+    @Parameters(name = "{0}")
+    public static BufferType[] data() {
+        return new BufferType[] { BufferType.HEAP, BufferType.DIRECT };
+    }
+
+    @Parameter
+    public BufferType bufferType;
+
+    @Test
+    public void checkNotNull() {
+        for (int[] sizes : TEST_SIZES) {
+            BufferUtils.checkNotNull(bufferType.newRandomBuffers(sizes));
+        }
+
+        ByteBuffer[] buffers = bufferType.newRandomBuffers(10, 10, 10, 10, 10);
+        buffers[2] = null;
+        try {
+            BufferUtils.checkNotNull(buffers);
+            fail();
+        } catch (IllegalArgumentException e) {
+            // Expected
+        }
+    }
+
+    @Test
+    public void remaining() {
+        for (int[] sizes : TEST_SIZES) {
+            assertEquals(arraySum(sizes),
+                    BufferUtils.remaining(bufferType.newRandomBuffers(sizes)));
+        }
+    }
+
+    @Test
+    public void consume() {
+        for (int[] sizes : TEST_SIZES) {
+            ByteBuffer[] buffers = bufferType.newRandomBuffers(sizes);
+            int totalSize = arraySum(sizes);
+
+            BufferUtils.consume(buffers, 0);
+            assertEquals(totalSize, BufferUtils.remaining(buffers));
+
+            BufferUtils.consume(buffers,totalSize / 2);
+            assertEquals(totalSize / 2, BufferUtils.remaining(buffers));
+
+            BufferUtils.consume(buffers,totalSize / 2);
+            assertEquals(0, BufferUtils.remaining(buffers));
+
+            if (totalSize > 0) {
+                try {
+                    BufferUtils.consume(buffers, totalSize / 2);
+                    fail("Managed to consume past end of buffer array");
+                } catch (IllegalArgumentException e) {
+                    // Expected
+                }
+            }
+        }
+    }
+
+    @Test
+    public void copyNoConsume() {
+        for (BufferType destinationType : BufferType.values()) {
+            for (int[] sizes : TEST_SIZES) {
+                ByteBuffer[] buffers = bufferType.newRandomBuffers(sizes);
+                int totalSize = arraySum(sizes);
+
+                ByteBuffer destination = destinationType.newBuffer(totalSize);
+                BufferUtils.copyNoConsume(buffers, destination, totalSize);
+                assertEquals(totalSize, BufferUtils.remaining(buffers));
+
+                assertArrayEquals(toArray(buffers), toArray(destination));
+            }
+        }
+    }
+
+    private static byte[] toArray(ByteBuffer... buffers) {
+        byte[] bytes = new byte[(int) BufferUtils.remaining(buffers)];
+        int offset = 0;
+        for (ByteBuffer buffer : buffers) {
+            int length = buffer.remaining();
+            if (length > 0) {
+                buffer.get(bytes, offset, length);
+                offset += length;
+            }
+        }
+        return bytes;
+    }
+
+    @Test
+    public void getBufferLargerThan_allSmall() {
+        ByteBuffer[] buffers = bufferType.newRandomBuffers(100, 200, 300, 400);
+
+        assertNull(BufferUtils.getBufferLargerThan(buffers, K16));
+
+        BufferUtils.consume(buffers, 300);
+        assertNull(BufferUtils.getBufferLargerThan(buffers, K16));
+        assertSame(buffers[2], BufferUtils.getBufferLargerThan(buffers, 100));
+
+        BufferUtils.consume(buffers, 300);
+        assertSame(buffers[3], BufferUtils.getBufferLargerThan(buffers, K16));
+
+        BufferUtils.consume(buffers, 200);
+        assertSame(buffers[3], BufferUtils.getBufferLargerThan(buffers, K16));
+
+        BufferUtils.consume(buffers, 200);
+        ByteBuffer buffer = BufferUtils.getBufferLargerThan(buffers, K16);
+        assertNull(buffer);
+    }
+
+    @Test
+    public void getBufferLargerThan_oneLarge() {
+        ByteBuffer[] buffers = bufferType.newRandomBuffers(100, K64, 300, 400);
+
+        assertNull(BufferUtils.getBufferLargerThan(buffers, K16));
+
+        BufferUtils.consume(buffers, 100);
+        assertSame(buffers[1], BufferUtils.getBufferLargerThan(buffers, K16));
+
+        BufferUtils.consume(buffers, 1024); // 63K remaining in buffers[1]
+        assertSame(buffers[1], BufferUtils.getBufferLargerThan(buffers, K16));
+
+        BufferUtils.consume(buffers, 60 * 1024); // 3K remaining in buffers[1]
+        assertNull(BufferUtils.getBufferLargerThan(buffers, K16));
+
+        BufferUtils.consume(buffers, 3 * 1024);
+        assertEquals(0, buffers[1].remaining());
+        assertNull(BufferUtils.getBufferLargerThan(buffers, K16));
+
+        BufferUtils.consume(buffers, 300);
+        assertSame(buffers[3], BufferUtils.getBufferLargerThan(buffers, K16));
+
+        BufferUtils.consume(buffers, 400);
+        ByteBuffer buffer = BufferUtils.getBufferLargerThan(buffers, K16);
+        assertNull(buffer);
+    }
+
+    @Test
+    public void getBufferLargerThan_onlyOneBuffer() {
+        ByteBuffer[] buffers = bufferType.newRandomBuffers(0, 0, 100, 0, 0);
+
+        assertSame(buffers[2], BufferUtils.getBufferLargerThan(buffers, K16));
+    }
+
+    private int arraySum(int[] sizes) {
+        int sum = 0;
+        for (int i : sizes) {
+            sum += i;
+        }
+        return sum;
+    }
+}
