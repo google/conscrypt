ad1e0bb901aab2a0e42dbfffa5d5ffb2dd18f234
Pete Bentley <prb@google.com>
2021-04-07 18:22:16 +0100

    Minor error-prone fixes.

--- common/src/test/java/org/conscrypt/BufferUtilsTest.java
+++ common/src/test/java/org/conscrypt/BufferUtilsTest.java
@@ -18,10 +18,8 @@ package org.conscrypt;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.nio.ByteBuffer;
