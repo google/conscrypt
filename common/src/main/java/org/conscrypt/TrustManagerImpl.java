1ad7436cba9727084f02187489d0dff58383b07d
Zixuan Liu <nodeces@gmail.com>
2022-03-30 19:54:45 +0800

    Fix use the default hostname verifier
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>

--- common/src/main/java/org/conscrypt/TrustManagerImpl.java
+++ common/src/main/java/org/conscrypt/TrustManagerImpl.java
@@ -1003,6 +1003,9 @@ public final class TrustManagerImpl extends X509ExtendedTrustManager {
         if (hostnameVerifier != null) {
             return hostnameVerifier;
         }
+        if (defaultHostnameVerifier != null) {
+            return defaultHostnameVerifier;
+        }
         return Platform.getDefaultHostnameVerifier();
     }
 
