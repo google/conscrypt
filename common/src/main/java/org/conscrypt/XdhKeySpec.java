ecb34cdc8f3103df6ce97cb4a607490f7bf3a4c4
Jorge Saldivar <jorgesaldivar@google.com>
2023-08-11 15:59:37 +0000

    Append documentation to XdhKeySpec

--- common/src/main/java/org/conscrypt/XdhKeySpec.java
+++ common/src/main/java/org/conscrypt/XdhKeySpec.java
@@ -3,15 +3,24 @@ package org.conscrypt;
 import java.security.spec.KeySpec;
 
 /**
- * External Diffie–Hellman key spec holding the public or private key in its raw format.
+ * External Diffie–Hellman key spec holding a key which could be either a public or private key.
  */
 public final class XdhKeySpec implements KeySpec {
     private final byte[] key;
 
+    /**
+     * Creates an instance of {@link XdhKeySpec} by passing a public or private key in its raw
+     * format.
+     */
     public XdhKeySpec(byte[] key) {
         this.key = key;
     }
 
+    /**
+     * Returns the public or private key in its raw format.
+     *
+     * @return key in its raw format.
+     */
     public byte[] getKey() {
         return key;
     }
