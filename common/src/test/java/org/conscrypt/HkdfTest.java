2fa51dbba75f592b0042be78409f212437fcdafb
Pete Bentley <prb@google.com>
2023-09-12 09:18:58 +0100

    Add missing static import.
    Unsure how this slipped through!

--- common/src/test/java/org/conscrypt/HkdfTest.java
+++ common/src/test/java/org/conscrypt/HkdfTest.java
@@ -17,6 +17,7 @@
 package org.conscrypt;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
