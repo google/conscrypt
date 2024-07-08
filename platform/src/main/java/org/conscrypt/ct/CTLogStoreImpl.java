98f0f2b13aa77024195f062c1ef8e22776cec583
Thiébaud Weksteen <tweek@google.com>
2024-07-08 12:29:21 +1000

    Support parsing CT v3 JSON log list
    Change the existing parsing to support the version 3 of CT log list
    schema (as available on www.gstatic.com/ct/log_list/v3/log_list.json).
    
    The parsing is triggered at the first call to getKnownLog.
    
    The parsing only populates existing attributes of CTLogInfo. Further
    parsing will be required to capture all the data for validation against
    the policy.
    
    Bug: 319829948
    Test: atest CtsLibcoreTestCases:com.android.org.conscrypt.ct.CTLogStoreImplTest
    Change-Id: I9159974e43dffc9d3c99efe88846db5a7843d855

--- platform/src/main/java/org/conscrypt/ct/CTLogStoreImpl.java
+++ platform/src/main/java/org/conscrypt/ct/CTLogStoreImpl.java
@@ -17,193 +17,164 @@
 package org.conscrypt.ct;
 
 import static java.nio.charset.StandardCharsets.US_ASCII;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
-import java.util.Arrays;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.conscrypt.Internal;
 import org.conscrypt.OpenSSLKey;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @Internal
 public class CTLogStoreImpl implements CTLogStore {
-    /**
-     * Thrown when parsing of a log file fails.
-     */
-    public static class InvalidLogFileException extends Exception {
-        public InvalidLogFileException() {
-        }
+    private static final Logger logger = Logger.getLogger(CTLogStoreImpl.class.getName());
+    public static final String V3_PATH = "/misc/keychain/ct/v3/log_list.json";
+    private static final Path defaultLogList;
 
-        public InvalidLogFileException(String message) {
-            super(message);
-        }
-
-        public InvalidLogFileException(String message, Throwable cause) {
-            super(message, cause);
-        }
-
-        public InvalidLogFileException(Throwable cause) {
-            super(cause);
-        }
-    }
-
-    private static final File defaultLogDir;
     static {
         String ANDROID_DATA = System.getenv("ANDROID_DATA");
-        defaultLogDir = new File(ANDROID_DATA + "/misc/keychain/trusted_ct_logs/current/");
+        defaultLogList = Paths.get(ANDROID_DATA, V3_PATH);
     }
 
-    private final File logDir;
-    private final CTLogInfo[] extraLogs;
+    private enum State {
+        UNINITIALIZED,
+        LOADED,
+        NOT_FOUND,
+        MALFORMED,
+    }
 
-    private final HashMap<ByteBuffer, CTLogInfo> logCache = new HashMap<>();
-    private final Set<ByteBuffer> missingLogCache
-            = Collections.synchronizedSet(new HashSet<ByteBuffer>());
+    private final Path logList;
+    private State state;
+    private String version;
+    private Map<ByteBuffer, CTLogInfo> logs;
 
     public CTLogStoreImpl() {
-        this(defaultLogDir, null);
+        this(defaultLogList);
     }
 
-    public CTLogStoreImpl(File logDir, CTLogInfo[] extraLogs) {
-        this.logDir = logDir;
-        this.extraLogs = extraLogs;
+    public CTLogStoreImpl(Path logList) {
+        this.state = State.UNINITIALIZED;
+        this.logList = logList;
     }
 
     @Override
     public CTLogInfo getKnownLog(byte[] logId) {
+        if (logId == null) {
+            return null;
+        }
+        if (!ensureLogListIsLoaded()) {
+            return null;
+        }
         ByteBuffer buf = ByteBuffer.wrap(logId);
-        CTLogInfo log = logCache.get(buf);
+        CTLogInfo log = logs.get(buf);
         if (log != null) {
             return log;
-        }
-        if (missingLogCache.contains(buf)) {
-            return null;
-        }
-
-        log = findKnownLog(logId);
-        if (log != null) {
-            logCache.put(buf, log);
-        } else {
-            missingLogCache.add(buf);
-        }
-
-        return log;
-    }
-
-    private CTLogInfo findKnownLog(byte[] logId) {
-        String filename = hexEncode(logId);
-        try {
-            return loadLog(new File(logDir, filename));
-        } catch (InvalidLogFileException e) {
-            return null;
-        } catch (FileNotFoundException e) {
-            // Ignored
-        }
-
-        for (CTLogInfo log : extraLogs) {
-            if (Arrays.equals(logId, log.getID())) {
-                return log;
-            }
         }
         return null;
     }
 
-    /**
-     * Load a CTLogInfo from a file.
-     * @throws FileNotFoundException if the file does not exist
-     * @throws InvalidLogFileException if the file could not be parsed properly
-     * @return a CTLogInfo or null if the file is empty
+    /* Ensures the log list is loaded.
+     * Returns true if the log list is usable.
      */
-    public static CTLogInfo loadLog(File file) throws FileNotFoundException,
-                                                      InvalidLogFileException {
-        return loadLog(new FileInputStream(file));
+    private boolean ensureLogListIsLoaded() {
+        synchronized (this) {
+            if (state == State.UNINITIALIZED) {
+                state = loadLogList();
+            }
+            return state == State.LOADED;
+        }
     }
 
-    /**
-     * Load a CTLogInfo from a textual representation. Closes {@code input} upon completion
-     * of loading.
-     *
-     * @throws InvalidLogFileException if the input could not be parsed properly
-     * @return a CTLogInfo or null if the input is empty
-     */
-    public static CTLogInfo loadLog(InputStream input) throws InvalidLogFileException {
-        final Scanner scan = new Scanner(input, "UTF-8");
-        scan.useDelimiter("\n");
-
-        String description = null;
-        String url = null;
-        String key = null;
+    private State loadLogList() {
+        byte[] content;
         try {
-            // If the scanner can't even read one token then the file must be empty/blank
-            if (!scan.hasNext()) {
-                return null;
-            }
-
-            while (scan.hasNext()) {
-                String[] parts = scan.next().split(":", 2);
-                if (parts.length < 2) {
-                    continue;
-                }
-
-                String name = parts[0];
-                String value = parts[1];
-                switch (name) {
-                    case "description":
-                        description = value;
-                        break;
-                    case "url":
-                        url = value;
-                        break;
-                    case "key":
-                        key = value;
-                        break;
-                }
-            }
-        } finally {
-            scan.close();
+            content = Files.readAllBytes(logList);
+        } catch (IOException e) {
+            return State.NOT_FOUND;
         }
-
-        if (description == null || url == null || key == null) {
-            throw new InvalidLogFileException("Missing one of 'description', 'url' or 'key'");
+        if (content == null) {
+            return State.NOT_FOUND;
         }
+        JSONObject json;
+        try {
+            json = new JSONObject(new String(content, UTF_8));
+        } catch (JSONException e) {
+            logger.log(Level.WARNING, "Unable to parse log list", e);
+            return State.MALFORMED;
+        }
+        HashMap<ByteBuffer, CTLogInfo> logsMap = new HashMap<>();
+        try {
+            version = json.getString("version");
+            JSONArray operators = json.getJSONArray("operators");
+            for (int i = 0; i < operators.length(); i++) {
+                JSONObject operator = operators.getJSONObject(i);
+                String operatorName = operator.getString("name");
+                JSONArray logs = operator.getJSONArray("logs");
+                for (int j = 0; j < logs.length(); j++) {
+                    JSONObject log = logs.getJSONObject(j);
+                    String description = log.getString("description");
+                    byte[] logId = Base64.getDecoder().decode(log.getString("log_id"));
+                    PublicKey key = parsePubKey(log.getString("key"));
+                    JSONObject stateObject = log.getJSONObject("state");
+                    int logState = parseState(stateObject.keys().next());
+                    String url = log.getString("url");
+                    CTLogInfo logInfo = new CTLogInfo(key, logState, description, url);
+                    logsMap.put(ByteBuffer.wrap(logId), logInfo);
+                }
+            }
+        } catch (JSONException | IllegalArgumentException e) {
+            logger.log(Level.WARNING, "Unable to parse log list", e);
+            return State.MALFORMED;
+        }
+        this.logs = Collections.unmodifiableMap(logsMap);
+        return State.LOADED;
+    }
 
+    private static int parseState(String state) {
+        switch (state) {
+            case "pending":
+                return CTLogInfo.STATE_PENDING;
+            case "qualified":
+                return CTLogInfo.STATE_QUALIFIED;
+            case "usable":
+                return CTLogInfo.STATE_USABLE;
+            case "readonly":
+                return CTLogInfo.STATE_READONLY;
+            case "retired":
+                return CTLogInfo.STATE_RETIRED;
+            case "rejected":
+                return CTLogInfo.STATE_REJECTED;
+            default:
+                throw new IllegalArgumentException("Unknown log state: " + state);
+        }
+    }
+
+    private static PublicKey parsePubKey(String key) {
         byte[] pem = ("-----BEGIN PUBLIC KEY-----\n" + key + "\n-----END PUBLIC KEY-----")
                              .getBytes(US_ASCII);
         PublicKey pubkey;
         try {
             pubkey = OpenSSLKey.fromPublicKeyPemInputStream(new ByteArrayInputStream(pem))
                              .getPublicKey();
-        } catch (InvalidKeyException e) {
-            throw new InvalidLogFileException(e);
-        } catch (NoSuchAlgorithmException e) {
-            throw new InvalidLogFileException(e);
+        } catch (InvalidKeyException | NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException(e);
         }
-
-        return new CTLogInfo(pubkey, CTLogInfo.STATE_USABLE, description, url);
-    }
-
-    private final static char[] HEX_DIGITS = new char[] {
-        '0', '1', '2', '3', '4', '5', '6', '7',
-        '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
-    };
-
-    private static String hexEncode(byte[] data) {
-        StringBuilder sb = new StringBuilder(data.length * 2);
-        for (byte b: data) {
-            sb.append(HEX_DIGITS[(b >> 4) & 0x0f]);
-            sb.append(HEX_DIGITS[b & 0x0f]);
-        }
-        return sb.toString();
+        return pubkey;
     }
 }
--- platform/src/test/java/org/conscrypt/ct/CTLogStoreImplTest.java
+++ platform/src/test/java/org/conscrypt/ct/CTLogStoreImplTest.java
@@ -19,148 +19,117 @@ package org.conscrypt.ct;
 import static java.nio.charset.StandardCharsets.US_ASCII;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.security.PublicKey;
+import java.util.Base64;
 import junit.framework.TestCase;
 import org.conscrypt.OpenSSLKey;
 
 public class CTLogStoreImplTest extends TestCase {
-    private static final String[] LOG_KEYS = new String[] {
-        "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEmXg8sUUzwBYaWrRb+V0IopzQ6o3U" +
-        "yEJ04r5ZrRXGdpYM8K+hB0pXrGRLI0eeWz+3skXrS0IO83AhA3GpRL6s6w==",
+    public void test_loadLogList() throws Exception {
+        // clang-format off
+        String content = "" +
+"{" +
+"  \"version\": \"1.1\"," +
+"  \"log_list_timestamp\": \"2024-01-01T11:55:12Z\"," +
+"  \"operators\": [" +
+"    {" +
+"      \"name\": \"Operator 1\"," +
+"      \"email\": [\"ct@operator1.com\"]," +
+"      \"logs\": [" +
+"        {" +
+"          \"description\": \"Operator 1 'Test2024' log\"," +
+"          \"log_id\": \"7s3QZNXbGs7FXLedtM0TojKHRny87N7DUUhZRnEftZs=\"," +
+"          \"key\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEHblsqctplMVc5ramA7vSuNxUQxcomQwGAVAdnWTAWUYr3MgDHQW0LagJ95lB7QT75Ve6JgT2EVLOFGU7L3YrwA==\"," +
+"          \"url\": \"https://operator1.example.com/logs/test2024/\"," +
+"          \"mmd\": 86400," +
+"          \"state\": {" +
+"            \"usable\": {" +
+"              \"timestamp\": \"2022-11-01T18:54:00Z\"" +
+"            }" +
+"          }," +
+"          \"temporal_interval\": {" +
+"            \"start_inclusive\": \"2024-01-01T00:00:00Z\"," +
+"            \"end_exclusive\": \"2025-01-01T00:00:00Z\"" +
+"          }" +
+"        }," +
+"        {" +
+"          \"description\": \"Operator 1 'Test2025' log\"," +
+"          \"log_id\": \"TnWjJ1yaEMM4W2zU3z9S6x3w4I4bjWnAsfpksWKaOd8=\"," +
+"          \"key\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEIIKh+WdoqOTblJji4WiH5AltIDUzODyvFKrXCBjw/Rab0/98J4LUh7dOJEY7+66+yCNSICuqRAX+VPnV8R1Fmg==\"," +
+"          \"url\": \"https://operator1.example.com/logs/test2025/\"," +
+"          \"mmd\": 86400," +
+"          \"state\": {" +
+"            \"usable\": {" +
+"              \"timestamp\": \"2023-11-26T12:00:00Z\"" +
+"            }" +
+"          }," +
+"          \"temporal_interval\": {" +
+"            \"start_inclusive\": \"2025-01-01T00:00:00Z\"," +
+"            \"end_exclusive\": \"2025-07-01T00:00:00Z\"" +
+"          }" +
+"        }" +
+"      ]" +
+"    }," +
+"    {" +
+"      \"name\": \"Operator 2\"," +
+"      \"email\": [\"ct@operator2.com\"]," +
+"      \"logs\": [" +
+"        {" +
+"          \"description\": \"Operator 2 'Test2024' Log\"," +
+"          \"log_id\": \"2ra/az+1tiKfm8K7XGvocJFxbLtRhIU0vaQ9MEjX+6s=\"," +
+"          \"key\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEd7Gbe4/mizX+OpIpLayKjVGKJfyTttegiyk3cR0zyswz6ii5H+Ksw6ld3Ze+9p6UJd02gdHrXSnDK0TxW8oVSA==\"," +
+"          \"url\": \"https://operator2.example.com/logs/test2024/\"," +
+"          \"mmd\": 86400," +
+"          \"state\": {" +
+"            \"usable\": {" +
+"              \"timestamp\": \"2022-11-30T17:00:00Z\"" +
+"            }" +
+"          }," +
+"          \"temporal_interval\": {" +
+"            \"start_inclusive\": \"2024-01-01T00:00:00Z\"," +
+"            \"end_exclusive\": \"2025-01-01T00:00:00Z\"" +
+"          }" +
+"        }" +
+"      ]" +
+"    }" +
+"  ]" +
+"}";
+        // clang-format on
 
-        "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAErEULmlBnX9L/+AK20hLYzPMFozYx" +
-        "pP0Wm1ylqGkPEwuDKn9DSpNSOym49SN77BLGuAXu9twOW/qT+ddIYVBEIw==",
+        File logList = writeFile(content);
+        CTLogStore store = new CTLogStoreImpl(logList.toPath());
 
-        "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEP6PGcXmjlyCBz2ZFUuUjrgbZLaEF" +
-        "gfLUkt2cEqlSbb4vTuB6WWmgC9h0L6PN6JF0CPcajpBKGlTI15242a8d4g==",
+        assertNull("A null logId should return null", store.getKnownLog(null));
 
-        "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAER3qB0NADsP1szXxe4EagrD/ryPVh" +
-        "Y/azWbKyXcK12zhXnO8WH2U4QROVUMctFXLflIzw0EivdRN9t7UH1Od30w==",
-
-        "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEY0ww9JqeJvzVtKNTPVb3JZa7s0ZV" +
-        "duH3PpshpMS5XVoPRSjSQCph6f3HjUcM3c4N2hpa8OFbrFFy37ttUrgD+A=="
-    };
-    private static final String[] LOG_FILENAMES = new String[] {
-        "df1c2ec11500945247a96168325ddc5c7959e8f7c6d388fc002e0bbd3f74d764",
-        "84f8ae3f613b13407a75fa2893b93ab03b18d86c455fe7c241ae020033216446",
-        "89baa01a445100009d8f9a238947115b30702275aafee675a7d94b6b09287619",
-        "57456bffe268e49a190dce4318456034c2b4958f3c0201bed5a366737d1e74ca",
-        "896c898ced4b8e6547fa351266caae4ca304f1c1ec2b623c2ee259c5452147b0"
-    };
-
-    private static final CTLogInfo[] LOGS;
-    private static final String[] LOGS_SERIALIZED;
-
-    static {
-        try {
-            int logCount = LOG_KEYS.length;
-            LOGS = new CTLogInfo[logCount];
-            LOGS_SERIALIZED = new String[logCount];
-            for (int i = 0; i < logCount; i++) {
-                byte[] pem = ("-----BEGIN PUBLIC KEY-----\n" + LOG_KEYS[i]
-                        + "\n-----END PUBLIC KEY-----\n")
-                                     .getBytes(US_ASCII);
-                ByteArrayInputStream is = new ByteArrayInputStream(pem);
-                PublicKey key = OpenSSLKey.fromPublicKeyPemInputStream(is).getPublicKey();
-                String description = String.format("Test Log %d", i);
-                String url = String.format("log%d.example.com", i);
-                LOGS[i] = new CTLogInfo(key, CTLogInfo.STATE_USABLE, description, url);
-                LOGS_SERIALIZED[i] = String.format("description:%s\nurl:%s\nkey:%s",
-                    description, url, LOG_KEYS[i]);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        byte[] pem = ("-----BEGIN PUBLIC KEY-----\n"
+                + "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEHblsqctplMVc5ramA7vSuNxUQxcomQwGAVAdnWTAWUYr"
+                + "3MgDHQW0LagJ95lB7QT75Ve6JgT2EVLOFGU7L3YrwA=="
+                + "\n-----END PUBLIC KEY-----\n")
+                             .getBytes(US_ASCII);
+        ByteArrayInputStream is = new ByteArrayInputStream(pem);
+        PublicKey key = OpenSSLKey.fromPublicKeyPemInputStream(is).getPublicKey();
+        String description = "Operator 1 'Test2024' log";
+        String url = "https://operator1.example.com/logs/test2024/";
+        CTLogInfo log1 = new CTLogInfo(key, CTLogInfo.STATE_USABLE, description, url);
+        byte[] log1Id = Base64.getDecoder().decode("7s3QZNXbGs7FXLedtM0TojKHRny87N7DUUhZRnEftZs=");
+        assertEquals("An existing logId should be returned", log1, store.getKnownLog(log1Id));
     }
 
-    public void test_loadLog() throws Exception {
-        CTLogInfo log = CTLogStoreImpl.loadLog(
-                new ByteArrayInputStream(LOGS_SERIALIZED[0].getBytes(US_ASCII)));
-        assertEquals(LOGS[0], log);
-
-        File testFile = writeFile(LOGS_SERIALIZED[0]);
-        log = CTLogStoreImpl.loadLog(testFile);
-        assertEquals(LOGS[0], log);
-
-        // Empty log file, used to mask fallback logs
-        assertEquals(null, CTLogStoreImpl.loadLog(new ByteArrayInputStream(new byte[0])));
-        try {
-            CTLogStoreImpl.loadLog(new ByteArrayInputStream("randomgarbage".getBytes(US_ASCII)));
-            fail("InvalidLogFileException not thrown");
-        } catch (CTLogStoreImpl.InvalidLogFileException e) {}
-
-        try {
-            CTLogStoreImpl.loadLog(new File("/nonexistent"));
-            fail("FileNotFoundException not thrown");
-        } catch (FileNotFoundException e) {}
-    }
-
-    public void test_getKnownLog() throws Exception {
-        File userDir = createTempDirectory();
-        userDir.deleteOnExit();
-
-        CTLogInfo[] extraLogs = new CTLogInfo[] {LOGS[2], LOGS[3]};
-
-        CTLogStore store = new CTLogStoreImpl(userDir, extraLogs);
-
-        /* Add logs 0 and 1 to the user and system directories respectively
-         * Log 2 & 3 are part of the extras.
-         * Log 4 is not in the store
-         */
-        File log0File = new File(userDir, LOG_FILENAMES[0]);
-        File log4File = new File(userDir, LOG_FILENAMES[4]);
-
-        writeFile(log0File, LOGS_SERIALIZED[0]);
-
-        // Logs 01 are present, log 2 is in the fallback and unused, log 3 is present but masked,
-        // log 4 is missing
-        assertEquals(LOGS[0], store.getKnownLog(LOGS[0].getID()));
-        assertEquals(LOGS[2], store.getKnownLog(LOGS[2].getID()));
-        assertEquals(LOGS[3], store.getKnownLog(LOGS[3].getID()));
-        assertEquals(null, store.getKnownLog(LOGS[4].getID()));
-    }
-
-    /**
-     * Create a temporary file and write to it.
-     * The file will be deleted on exit.
-     * @param contents The data to be written to the file
-     * @return A reference to the temporary file
-     */
-    private File writeFile(String contents) throws IOException {
+    private File writeFile(String content) throws IOException {
         File file = File.createTempFile("test", null);
         file.deleteOnExit();
-        writeFile(file, contents);
-        return file;
-    }
-
-    private static void writeFile(File file, String contents) throws FileNotFoundException {
-        PrintWriter writer = new PrintWriter(
-                new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), UTF_8)),
-                false);
-        try {
-            writer.write(contents);
-        } finally {
-            writer.close();
+        try (FileWriter fw = new FileWriter(file)) {
+            fw.write(content);
         }
-    }
-
-    /*
-     * This is NOT safe, as another process could create a file between delete() and mkdir()
-     * It should be fine for tests though
-     */
-    private static File createTempDirectory() throws IOException {
-        File folder = File.createTempFile("test", "");
-        folder.delete();
-        folder.mkdir();
-        return folder;
+        return file;
     }
 }
 
