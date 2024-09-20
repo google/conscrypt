7249cb39ffb4da8a9d8af266e1024c47942adf0e
Pete Bentley <prb@google.com>
2024-09-20 11:30:04 +0100

    Errata from upstreaming and downstreaming.
    Couple of missing public modifiers and Kotlin reformatted to
    Google standard.

--- api-doclet/src/main/kotlin/org/conscrypt/doclet/FilterDoclet.kt
+++ api-doclet/src/main/kotlin/org/conscrypt/doclet/FilterDoclet.kt
@@ -17,9 +17,6 @@
 package org.conscrypt.doclet
 
 import com.sun.source.util.DocTrees
-import jdk.javadoc.doclet.Doclet
-import jdk.javadoc.doclet.DocletEnvironment
-import jdk.javadoc.doclet.Reporter
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -27,128 +24,104 @@ import java.util.Locale
 import javax.lang.model.SourceVersion
 import javax.lang.model.util.Elements
 import javax.lang.model.util.Types
+import jdk.javadoc.doclet.Doclet
+import jdk.javadoc.doclet.DocletEnvironment
+import jdk.javadoc.doclet.Reporter
 
 class FilterDoclet : Doclet {
-    companion object {
-        lateinit var docTrees: DocTrees
-        lateinit var elementUtils: Elements
-        lateinit var typeUtils: Types
-        lateinit var outputPath: Path
-        var baseUrl: String = "https://docs.oracle.com/javase/8/docs/api/"
-        val CSS_FILENAME = "styles.css"
-        var outputDir = "."
-        var docTitle = "DTITLE"
-        var windowTitle = "WTITLE"
-        var noTimestamp: Boolean = false
-        val classIndex = ClassIndex()
+  companion object {
+    lateinit var docTrees: DocTrees
+    lateinit var elementUtils: Elements
+    lateinit var typeUtils: Types
+    lateinit var outputPath: Path
+    var baseUrl: String = "https://docs.oracle.com/javase/8/docs/api/"
+    val CSS_FILENAME = "styles.css"
+    var outputDir = "."
+    var docTitle = "DTITLE"
+    var windowTitle = "WTITLE"
+    var noTimestamp: Boolean = false
+    val classIndex = ClassIndex()
+  }
+
+  override fun init(locale: Locale?, reporter: Reporter?) = Unit // TODO
+
+  override fun getName() = "FilterDoclet"
+
+  override fun getSupportedSourceVersion() = SourceVersion.latest()
+
+  override fun run(environment: DocletEnvironment): Boolean {
+    docTrees = environment.docTrees
+    elementUtils = environment.elementUtils
+    typeUtils = environment.typeUtils
+    outputPath = Paths.get(outputDir)
+    Files.createDirectories(outputPath)
+
+    classIndex.addVisible(environment.includedElements)
+
+    try {
+      generateClassFiles()
+      generateIndex()
+      return true
+    } catch (e: Exception) {
+      System.err.println("Error generating documentation: " + e.message)
+      e.printStackTrace()
+      return false
     }
+  }
 
-    override fun init(locale: Locale?, reporter: Reporter?) = Unit // TODO
-    override fun getName() = "FilterDoclet"
-    override fun getSupportedSourceVersion() = SourceVersion.latest()
+  private fun generateClassFiles() = classIndex.classes().forEach(::generateClassFile)
 
-    override fun run(environment: DocletEnvironment): Boolean {
-        docTrees = environment.docTrees
-        elementUtils = environment.elementUtils
-        typeUtils = environment.typeUtils
-        outputPath = Paths.get(outputDir)
-        Files.createDirectories(outputPath)
+  private fun generateIndex() {
+    val indexPath = outputPath.resolve("index.html")
 
-        classIndex.addVisible(environment.includedElements)
-
-        try {
-            generateClassFiles()
-            generateIndex()
-            return true
-        } catch (e: Exception) {
-            System.err.println("Error generating documentation: " + e.message)
-            e.printStackTrace()
-            return false
+    html {
+        body(title = docTitle, stylesheet = relativePath(indexPath, CSS_FILENAME)) {
+          div("index-container") {
+            h1(docTitle, "index-title")
+            compose { classIndex.generateHtml() }
+          }
         }
-    }
+      }
+      .let { Files.newBufferedWriter(indexPath).use { writer -> writer.write(it) } }
+  }
 
-    private fun generateClassFiles() = classIndex.classes().forEach(::generateClassFile)
+  private fun generateClassFile(classInfo: ClassInfo) {
+    val classFilePath = outputPath.resolve(classInfo.fileName)
+    Files.createDirectories(classFilePath.parent)
+    val simpleName = classInfo.simpleName
 
-    private fun generateIndex() {
-        val indexPath = outputPath.resolve("index.html")
-
-        html {
-            body(
-                title = docTitle,
-                stylesheet = relativePath(indexPath, CSS_FILENAME),
-            ) {
-                div("index-container") {
-                    h1(docTitle, "index-title")
-                    compose {
-                        classIndex.generateHtml()
-                    }
-                }
-            }
-        }.let {
-            Files.newBufferedWriter(indexPath).use { writer ->
-                writer.write(it)
-            }
+    html {
+        body(
+          title = "$simpleName - conscrypt-openjdk API",
+          stylesheet = relativePath(classFilePath, CSS_FILENAME),
+        ) {
+          compose { classInfo.generateHtml() }
         }
+      }
+      .let { Files.newBufferedWriter(classFilePath).use { writer -> writer.write(it) } }
+  }
+
+  private fun relativePath(from: Path, to: String): String {
+    val fromDir = from.parent
+    val toPath = Paths.get(outputDir).resolve(to)
+
+    if (fromDir == null) {
+      return to
     }
 
-    private fun generateClassFile(classInfo: ClassInfo) {
-        val classFilePath = outputPath.resolve(classInfo.fileName)
-        Files.createDirectories(classFilePath.parent)
-        val simpleName = classInfo.simpleName
+    val relativePath = fromDir.relativize(toPath)
+    return relativePath.toString().replace('\\', '/')
+  }
 
-        html {
-            body(
-                title = "$simpleName - conscrypt-openjdk API",
-                stylesheet = relativePath(classFilePath, CSS_FILENAME),
-            ) {
-                compose {
-                    classInfo.generateHtml()
-                }
-            }
-        }.let {
-            Files.newBufferedWriter(classFilePath).use { writer ->
-                writer.write(it)
-            }
-        }
-    }
-
-    private fun relativePath(from: Path, to: String): String {
-        val fromDir = from.parent
-        val toPath = Paths.get(outputDir).resolve(to)
-
-        if (fromDir == null) {
-            return to
-        }
-
-        val relativePath = fromDir.relativize(toPath)
-        return relativePath.toString().replace('\\', '/')
-    }
-
-    override fun getSupportedOptions(): Set<Doclet.Option> {
-        return setOf<Doclet.Option>(
-            StringOption(
-                "-d",
-                "<directory>",
-                "Destination directory for output files"
-            ) { d: String -> outputDir = d },
-            StringOption(
-                "-doctitle",
-                "<title>",
-                "Document title"
-            ) { t: String -> docTitle = t },
-            StringOption(
-                "-windowtitle",
-                "<title>",
-                "Window title"
-            ) { w: String -> windowTitle = w },
-            StringOption(
-                "-link",
-                "<link>",
-                "Link"
-            ) { l: String -> baseUrl = l },
-            BooleanOption(
-                "-notimestamp",
-                "Something"
-            ) { noTimestamp = true })
-    }
+  override fun getSupportedOptions(): Set<Doclet.Option> {
+    return setOf<Doclet.Option>(
+      StringOption("-d", "<directory>", "Destination directory for output files") { d: String ->
+        outputDir = d
+      },
+      StringOption("-doctitle", "<title>", "Document title") { t: String -> docTitle = t },
+      StringOption("-windowtitle", "<title>", "Window title") { w: String -> windowTitle = w },
+      StringOption("-link", "<link>", "Link") { l: String -> baseUrl = l },
+      BooleanOption("-notimestamp", "Something") { noTimestamp = true },
+    )
+  }
 }
--- api-doclet/src/main/kotlin/org/conscrypt/doclet/Options.kt
+++ api-doclet/src/main/kotlin/org/conscrypt/doclet/Options.kt
@@ -16,38 +16,43 @@
 
 package org.conscrypt.doclet
 
-import jdk.javadoc.doclet.Doclet.Option
 import java.util.function.Consumer
+import jdk.javadoc.doclet.Doclet.Option
 
 abstract class BaseOption(private val name: String) : Option {
-    override  fun getKind() = Option.Kind.STANDARD
-    override fun getNames(): List<String> = listOf(name)
+  override fun getKind() = Option.Kind.STANDARD
+
+  override fun getNames(): List<String> = listOf(name)
 }
 
-class StringOption(name: String,
-                   private val parameters: String,
-                   private val description: String,
-                   private val action: Consumer<String>
+class StringOption(
+  name: String,
+  private val parameters: String,
+  private val description: String,
+  private val action: Consumer<String>,
 ) : BaseOption(name) {
-    override fun getArgumentCount() = 1
-    override fun getDescription(): String = description
-    override fun getParameters(): String = parameters
+  override fun getArgumentCount() = 1
 
-    override fun process(option: String, arguments: MutableList<String>): Boolean {
-        action.accept(arguments[0])
-        return true
-    }
+  override fun getDescription(): String = description
+
+  override fun getParameters(): String = parameters
+
+  override fun process(option: String, arguments: MutableList<String>): Boolean {
+    action.accept(arguments[0])
+    return true
+  }
 }
 
-class BooleanOption(name: String,
-                    private val description: String,
-                    private val action: Runnable): BaseOption(name) {
-    override fun getArgumentCount() = 0
-    override fun getDescription(): String = description
-    override fun getParameters(): String = ""
+class BooleanOption(name: String, private val description: String, private val action: Runnable) :
+  BaseOption(name) {
+  override fun getArgumentCount() = 0
 
-    override fun process(option: String, arguments: MutableList<String>): Boolean {
-        action.run()
-        return true
-    }
+  override fun getDescription(): String = description
+
+  override fun getParameters(): String = ""
+
+  override fun process(option: String, arguments: MutableList<String>): Boolean {
+    action.run()
+    return true
+  }
 }
