4445fa5dedf1db1d60d3ebf495876cccc70b9f78
juergw <juerg@google.com>
2024-11-26 18:15:54 +0000

    Revert some unrelated changes.

--- common/src/main/java/org/conscrypt/OpenSSLAeadCipher.java
+++ common/src/main/java/org/conscrypt/OpenSSLAeadCipher.java
@@ -33,6 +33,11 @@ import javax.crypto.spec.IvParameterSpec;
 @Internal
 public abstract class OpenSSLAeadCipher extends OpenSSLCipher {
     /**
+     * Controls whether no-copy optimizations for direct ByteBuffers are enabled.
+     */
+    private static final boolean ENABLE_BYTEBUFFER_OPTIMIZATIONS = true;
+
+    /**
      * The default tag size when one is not specified. Default to
      * full-length tags (128-bits or 16 octets).
      */
@@ -223,7 +228,7 @@ public abstract class OpenSSLAeadCipher extends OpenSSLCipher {
     @Override
     protected int engineDoFinal(ByteBuffer input, ByteBuffer output) throws ShortBufferException,
             IllegalBlockSizeException, BadPaddingException {
-    if (!Conscrypt.getEnableBytebufferOptimizations()) {
+    if (!ENABLE_BYTEBUFFER_OPTIMIZATIONS) {
             return super.engineDoFinal(input, output);
         }
         if (input == null || output == null) {
