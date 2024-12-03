34840bd2763622d7fd512ead572251126ce88772
Miguel <miguelaranda@google.com>
2024-12-03 17:11:31 +0000

    Re-add stats-log class for conscrypt metrics
    This class is created by codegen in the gmscore version of conscrypt and so we need the logging path to go through it in order for it to be replaced in gmscore

--- common/src/main/java/org/conscrypt/metrics/ConscryptStatsLog.java
+++ common/src/main/java/org/conscrypt/metrics/ConscryptStatsLog.java
@@ -45,9 +45,9 @@ public final class ConscryptStatsLog {
     private ConscryptStatsLog() {}
 
     public static void write(int atomId, boolean success, int protocol, int cipherSuite,
-            int duration, Source source, int[] uids) {
+            int duration, Source source) {
         ReflexiveStatsEvent event = ReflexiveStatsEvent.buildEvent(
-                atomId, success, protocol, cipherSuite, duration, source.ordinal(), uids);
+                atomId, success, protocol, cipherSuite, duration, source.ordinal());
 
         ReflexiveStatsLog.write(event);
     }
