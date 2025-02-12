afb36f62a259aa96eb5f75c597a07f9c30182ced
miguelaranda0 <90468342+miguelaranda0@users.noreply.github.com>
2025-02-12 10:48:06 +0000

    Add tls deprecation status initialization. (#1304)
    Test: NativeCryptoTest

--- common/src/main/java/org/conscrypt/NativeCrypto.java
+++ common/src/main/java/org/conscrypt/NativeCrypto.java
@@ -69,6 +69,7 @@ public final class NativeCrypto {
             error = t;
         }
         loadError = error;
+        setTlsV1DeprecationStatus(Platform.isTlsV1Deprecated(), Platform.isTlsV1Supported());
     }
 
     private native static void clinit();
