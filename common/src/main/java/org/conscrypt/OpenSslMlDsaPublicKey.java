85c49f96669af754d411b7f510768eaa49f96f05
Jürg Wullschleger <52660988+juergw@users.noreply.github.com>
2025-02-24 20:36:33 +0100

    Add support for ML-DSA-65 signatures. (#1305)
    * Add support for ML-DSA-65.
    
    PKCS8 and X509 are not yet supported, because that is not yet standardized, see
    https://datatracker.ietf.org/doc/html/draft-ietf-lamps-dilithium-certificates-07
    
    It is better to not support this until it is standardized, so that we don't have
    to make a breaking change if the format still changes.
    
    Therefore, keys also can't yet be serialized.
    
    Raw serialization and deserialization is supported. For private keys, the seed
    is used as raw format. The expanded private key is not supported. BoringSSL
    doesn't provided a public API to serialise the expanded private key.

---
+++ common/src/main/java/org/conscrypt/OpenSslMlDsaPublicKey.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.conscrypt;
+
+import java.security.PublicKey;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+
+/** An OpenSSL ML-DSA public key. */
+public class OpenSslMlDsaPublicKey implements PublicKey {
+    private static final long serialVersionUID = 453861992373478445L;
+
+    private final byte[] raw;
+
+    public OpenSslMlDsaPublicKey(EncodedKeySpec keySpec) throws InvalidKeySpecException {
+        byte[] encoded = keySpec.getEncoded();
+        if ("raw".equalsIgnoreCase(keySpec.getFormat())) {
+            raw = encoded;
+        } else {
+            throw new InvalidKeySpecException("Encoding must be in raw format");
+        }
+    }
+
+    public OpenSslMlDsaPublicKey(byte[] raw) {
+        this.raw = raw.clone();
+    }
+
+    @Override
+    public String getAlgorithm() {
+        return "ML-DSA";
+    }
+
+    @Override
+    public String getFormat() {
+        throw new UnsupportedOperationException("getFormat() not yet supported");
+    }
+
+    @Override
+    public byte[] getEncoded() {
+        throw new UnsupportedOperationException("getEncoded() not yet supported");
+    }
+
+    byte[] getRaw() {
+        if (raw == null) {
+            throw new IllegalStateException("key is destroyed");
+        }
+        return raw.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (raw == null) {
+            throw new IllegalStateException("key is destroyed");
+        }
+
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof OpenSslMlDsaPublicKey)) {
+            return false;
+        }
+        OpenSslMlDsaPublicKey that = (OpenSslMlDsaPublicKey) o;
+        return Arrays.equals(raw, that.raw);
+    }
+
+    @Override
+    public int hashCode() {
+        if (raw == null) {
+            throw new IllegalStateException("key is destroyed");
+        }
+        return Arrays.hashCode(raw);
+    }
+}
