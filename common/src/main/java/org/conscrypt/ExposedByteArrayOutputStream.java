d6c3a208a0004b5a0882944fe1b15a0d21fe42c8
juergw <juerg@google.com>
2025-03-31 13:57:29 +0000

    Add subclass of ByteArrayOutputStream that exposes the underlying array.
    This is already used twice, and may be needed in other places in the future.
    It's better to move it into its own class.

---
+++ common/src/main/java/org/conscrypt/ExposedByteArrayOutputStream.java
@@ -0,0 +1,21 @@
+package org.conscrypt;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * ByteArrayOutputStream that exposes the underlying byte array.
+ */
+final class ExposedByteArrayOutputStream extends ByteArrayOutputStream {
+
+  ExposedByteArrayOutputStream() {
+    super();
+  }
+
+  ExposedByteArrayOutputStream(int initialCapacity) {
+    super(initialCapacity);
+  }
+
+  byte[] array() {
+      return buf;
+  }
+}
---
+++ common/src/test/java/org/conscrypt/ExposedByteArrayOutputStreamTest.java
@@ -0,0 +1,64 @@
+package org.conscrypt;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class ExposedByteArrayOutputStreamTest {
+
+  @Test
+  public void write_works() throws Exception {
+    ExposedByteArrayOutputStream outputStream = new ExposedByteArrayOutputStream();
+
+    outputStream.write(new byte[] { 1, 2, 3});
+    outputStream.write(new byte[] {127, 4, 5, 127}, /* off= */ 1, /* len= */ 2);
+    outputStream.write(6);
+
+    assertArrayEquals(new byte[] { 1, 2, 3, 4, 5, 6}, outputStream.toByteArray());
+    assertEquals(6, outputStream.size());
+  }
+
+  @Test
+  public void reset_works() throws Exception {
+    ExposedByteArrayOutputStream outputStream = new ExposedByteArrayOutputStream();
+    outputStream.write(new byte[] { 1, 2, 3});
+
+    outputStream.reset();
+    outputStream.write(new byte[] { 7, 8, 9});
+
+    assertArrayEquals(new byte[] { 7, 8, 9}, outputStream.toByteArray());
+    assertEquals(3, outputStream.size());
+  }
+
+  @Test
+  public void array_doesNotCopyArray() throws Exception {
+    ExposedByteArrayOutputStream outputStream =
+        new ExposedByteArrayOutputStream(/* initialCapacity= */ 6);
+    byte[] array = outputStream.array();
+    // Because array is not a copy, any write will change the array, if it is large enough.
+    outputStream.write(new byte[] { 1, 2, 3});
+    assertArrayEquals(new byte[] { 1, 2, 3, 0, 0, 0}, array);
+  }
+
+  @Test
+  public void createWithCapacity_works() throws Exception {
+    ExposedByteArrayOutputStream outputStream =
+        new ExposedByteArrayOutputStream(/* initialCapacity= */ 77);
+    assertEquals(77, outputStream.array().length);
+  }
+
+  @Test
+  public void capacityTooSmall_resizes() throws Exception  {
+    ExposedByteArrayOutputStream outputStream =
+        new ExposedByteArrayOutputStream(/* initialCapacity= */ 3);
+    assertEquals(3, outputStream.array().length);
+    outputStream.write(new byte[] { 1, 2, 3, 4});
+    assertTrue(outputStream.array().length > 3);
+  }
+}
