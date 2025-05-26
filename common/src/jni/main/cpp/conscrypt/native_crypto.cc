5bb8514f0d57aa735678a37c31670f551ec46101
mnbogner <mnbogner@eds.org>
2025-05-26 10:20:50 -0700

    revert changes required for local build issues

--- common/src/jni/main/cpp/conscrypt/native_crypto.cc
+++ common/src/jni/main/cpp/conscrypt/native_crypto.cc
@@ -7595,11 +7595,7 @@ static int sslSelect(JNIEnv* env, int type, jobject fdObject, AppData* appData,
         if (fds[1].revents & POLLIN) {
             char token;
             do {
-                int foo = 0;
-                foo = read(appData->fdsEmergency[0], &token, 1);
-                if (foo > 0) {
-                    CONSCRYPT_LOG_VERBOSE("FOO: %d", foo);
-                }
+                (void)read(appData->fdsEmergency[0], &token, 1);
             } while (errno == EINTR);
         }
     }
@@ -7630,11 +7626,7 @@ static void sslNotify(AppData* appData) {
     char token = '*';
     do {
         errno = 0;
-        int foo = 0;
-        foo = write(appData->fdsEmergency[1], &token, 1);
-        if (foo > 0) {
-            CONSCRYPT_LOG_VERBOSE("FOO: %d", foo);
-        }
+        (void)write(appData->fdsEmergency[1], &token, 1);
     } while (errno == EINTR);
     errno = errnoBackup;
 #endif
