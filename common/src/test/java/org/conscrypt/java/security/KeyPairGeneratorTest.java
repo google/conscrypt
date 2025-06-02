e48bf07d9d9cd5c9bc4d91b46c2eb28901ccede3
juergw <juerg@google.com>
2025-06-02 19:59:00 +0000

    Fix formatting.

--- common/src/test/java/org/conscrypt/java/security/KeyPairGeneratorTest.java
+++ common/src/test/java/org/conscrypt/java/security/KeyPairGeneratorTest.java
@@ -238,10 +238,10 @@ public class KeyPairGeneratorTest {
             expectedAlgorithm = "DH";
         }
         if (expectedAlgorithm.startsWith("ML-DSA")) {
-                // In OpenJDK, KeyPairGenerator.getInstance("ML-DSA-65") returns a
-                // KeyPairGenerator with algorithm "ML-DSA-65". But the key it generates
-                // have algorithm "ML-DSA".
-                expectedAlgorithm = "ML-DSA";
+            // In OpenJDK, KeyPairGenerator.getInstance("ML-DSA-65") returns a
+            // KeyPairGenerator with algorithm "ML-DSA-65". But the key it generates
+            // have algorithm "ML-DSA"s.
+            expectedAlgorithm = "ML-DSA";
         }
         assertEquals(expectedAlgorithm, k.getAlgorithm().toUpperCase(Locale.ROOT));
         if (expectedAlgorithm.equals("DH")) {
