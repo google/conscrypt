ee480e94eaae6a60a1d9c50f76e9929aa69887f1
Pete Bentley <pete@sorted.org>
2025-09-09 15:43:41 +0100

    Remove subprojects logic in favour of convention plugins.
    Should be a no-op for Gradle builds and definitely a no-op
    everywhere else.  I didn't manage to test signing still works
    but will do at some point.
    
    No changes in the logic of what is applied to each project and
    I've kept the convention plugins in Groovy for now for ease
    of review.
    
    This is unfortunately a slightly large change but should be
    straightforward to review and unlocks the path to other,
    smaller, incremental gradle improvements.
    
    Planned next steps (in no particular order):
    * Move the NativeBuildInfo logic to a plugin and support ARM Linux.
    * Simplify boringssl and property file logic
    * Migrate to a recent Gradle
    * Use recent Gradle toolchain logic to simplify native builds
    * Simplify JVM task logic

---
+++ build-logic/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    `kotlin-dsl`
+    `java-gradle-plugin`
+    id("groovy-gradle-plugin")
+}
+
+repositories {
+    mavenCentral()
+    google()
+    gradlePluginPortal()
+}
+
+// Annoyingly, build-logic is pre-built before the rest of the subprojects to cannot
+// make use of the version catalogue in libs.versions.toml.
+dependencies {
+    // AGP
+    implementation("com.android.tools.build:gradle:7.4.2")
+    implementation("biz.aQute.bnd:biz.aQute.bnd.gradle:6.4.0")
+    implementation("com.google.gradle:osdetector-gradle-plugin:1.7.3")
+    implementation("net.ltgt.gradle:gradle-errorprone-plugin:4.3.0")
+    implementation("org.ajoberstar.grgit:grgit-gradle:5.3.3")
+    implementation("com.dorongold.plugins:task-tree:3.0.0")
+    implementation(gradleApi())
+}
