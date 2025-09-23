82b5badb5170f0923ac9d499e7bd683cccb0095b
mnbogner <mnbogner@eds.org>
2025-09-23 12:05:25 -0700

    Removed several redundant "else" blocks.  Commented out non-ech changes to see if CI tests pass.

--- common/src/jni/main/cpp/conscrypt/native_crypto.cc
+++ common/src/jni/main/cpp/conscrypt/native_crypto.cc
@@ -1167,21 +1167,22 @@ static jint NativeCrypto_EVP_PKEY_cmp(JNIEnv* env, jclass, jobject pkey1Ref, job
     JNI_TRACE("EVP_PKEY_cmp(%p, %p)", pkey1Ref, pkey2Ref);
     EVP_PKEY* pkey1 = fromContextObject<EVP_PKEY>(env, pkey1Ref);
     if (pkey1 == nullptr) {
-        conscrypt::jniutil::throwNullPointerException(env, "Null pointer, key 1");
-        ERR_clear_error();
+        //conscrypt::jniutil::throwNullPointerException(env, "Null pointer, key 1");
+        //ERR_clear_error();
         JNI_TRACE("EVP_PKEY_cmp => pkey1 == null");
         return 0;
     }
     EVP_PKEY* pkey2 = fromContextObject<EVP_PKEY>(env, pkey2Ref);
     if (pkey2 == nullptr) {
-        conscrypt::jniutil::throwNullPointerException(env, "Null pointer, key 2");
-        ERR_clear_error();
+        //conscrypt::jniutil::throwNullPointerException(env, "Null pointer, key 2");
+        //ERR_clear_error();
         JNI_TRACE("EVP_PKEY_cmp => pkey2 == null");
         return 0;
     }
     JNI_TRACE("EVP_PKEY_cmp(%p, %p) <- ptr", pkey1, pkey2);
 
     int result = EVP_PKEY_cmp(pkey1, pkey2);
+    /*
     if (result < 0) {
         conscrypt::jniutil::throwInvalidKeyException(env, "Decoding error");
         ERR_clear_error();
@@ -1191,6 +1192,9 @@ static jint NativeCrypto_EVP_PKEY_cmp(JNIEnv* env, jclass, jobject pkey1Ref, job
         JNI_TRACE("EVP_PKEY_cmp(%p, %p) => %d", pkey1, pkey2, result);
         return result;
     }
+    */
+    JNI_TRACE("EVP_PKEY_cmp(%p, %p) => %d", pkey1, pkey2, result);
+    return result;
 }
 
 /*
@@ -8300,7 +8304,6 @@ static jlong NativeCrypto_SSL_CTX_new(JNIEnv* env, jclass) {
     bssl::UniquePtr<SSL_CTX> sslCtx(SSL_CTX_new(TLS_with_buffers_method()));
     if (sslCtx.get() == nullptr) {
         conscrypt::jniutil::throwExceptionFromBoringSSLError(env, "SSL_CTX_new");
-        ERR_clear_error();
         return 0;
     }
     SSL_CTX_set_options(
@@ -11846,10 +11849,10 @@ static jboolean NativeCrypto_SSL_set1_ech_config_list(JNIEnv* env, jclass, jlong
         ERR_clear_error();
         JNI_TRACE("ssl=%p NativeCrypto_SSL_set1_ech_config_list(%p) => threw exception", ssl, configJavaBytes);
         return JNI_FALSE;
-    } else {
-        JNI_TRACE("ssl=%p NativeCrypto_SSL_set1_ech_config_list(%p) => %d", ssl, configJavaBytes, ret);
-        return ret;
     }
+
+    JNI_TRACE("ssl=%p NativeCrypto_SSL_set1_ech_config_list(%p) => %d", ssl, configJavaBytes, ret);
+    return ret;
 }
 
 static jstring NativeCrypto_SSL_get0_ech_name_override(JNIEnv* env, jclass, jlong ssl_address,
@@ -11946,10 +11949,10 @@ static jboolean NativeCrypto_SSL_ech_accepted(JNIEnv* env, jclass, jlong ssl_add
         ERR_clear_error();
         JNI_TRACE("ssl=%p NativeCrypto_SSL_ech_accepted => threw exception", ssl);
         return JNI_FALSE;
-    } else {
-        JNI_TRACE("ssl=%p NativeCrypto_SSL_ech_accepted => %d", ssl, accepted);
-        return accepted;
     }
+
+    JNI_TRACE("ssl=%p NativeCrypto_SSL_ech_accepted => %d", ssl, accepted);
+    return accepted;
 }
 
 static jboolean NativeCrypto_SSL_CTX_ech_enable_server(JNIEnv* env, jclass, jlong ssl_ctx_address,
