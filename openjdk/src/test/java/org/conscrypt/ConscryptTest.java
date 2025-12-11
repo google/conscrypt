2eb42e5b74be42160a0535016048a8833d20960a
Miguel Aranda <miguelaranda@google.com>
2025-12-11 14:57:29 +0000

    Change imports

--- openjdk/src/test/java/org/conscrypt/ConscryptTest.java
+++ openjdk/src/test/java/org/conscrypt/ConscryptTest.java
@@ -22,15 +22,16 @@ import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.security.Provider;
-import java.security.Security;
-import javax.net.ssl.SSLContext;
-
 import org.conscrypt.java.security.StandardNames;
 // import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.security.Provider;
+import java.security.Security;
+
+import javax.net.ssl.SSLContext;
 
 @RunWith(JUnit4.class)
 public class ConscryptTest {
