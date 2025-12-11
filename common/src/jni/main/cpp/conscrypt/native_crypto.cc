5b0e8d15c515ea0761c8a06ea5e3c659ce74787e
juergw <juerg@google.com>
2025-12-11 14:23:14 +0000

    Use normal for loop.

--- common/src/jni/main/cpp/conscrypt/native_crypto.cc
+++ common/src/jni/main/cpp/conscrypt/native_crypto.cc
@@ -9882,8 +9882,9 @@ static void NativeCrypto_SSL_set1_groups(JNIEnv* env, jclass, jlong sslAddress,
     }
     std::vector<int> groups_vector;
     groups_vector.reserve(groups_ro.size());
-    for (jint group_id : groups_ro) {
-        groups_vector.push_back(group_id);
+    const jint* groups_ptr = groups_ro.get();
+    for (int i = 0; i < groups_ro.size(); i++) {
+        groups_vector.push_back(groups_ptr[i]);
     }
 
     if (!SSL_set1_groups(ssl, groups_vector.data(), groups_vector.size())) {
